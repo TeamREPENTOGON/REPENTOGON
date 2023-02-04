9d8c3c1eb0430d42e87b516d51d367a2da8751b9
jsgnextortex <62559750+jsgnextortex@users.noreply.github.com>
2023-02-03 23:06:32 -0300

    Add the AddItem callback to the api (with return overrides)
    You can return a table including the values you want for the 5 params, like the preentity spawn callback.

--- modtest/LuaTest.cpp
+++ modtest/LuaTest.cpp
@@ -22,3 +22,32 @@ HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
 	lua_register(state, "Lua_TestLua", Lua_TestLua);
 	printf("i'm repeotogonnning!!!!!!\n");
 };
+
+//AddCollectible Callback (id: 1004 enum pending)
+HOOK_METHOD(Entity_Player, AddCollectible, (int type, int charge, bool firsttime, int slot, int vardata) -> void) {
+	printf("item get");
+	lua_State *L = g_LuaEngine->_state;
+	
+	char luacmd[100];
+	sprintf(luacmd, "return Isaac.RunCallback(1004,%d,%d,%d,%d,%d)", type, charge,firsttime,slot,vardata); //this is not "the good way of doing it", but it's the cleanest looking
+
+	if ((!luaL_dostring(L,luacmd)) && (lua_istable(L, -1))) {
+		printf("Additem callback run \n");
+		int tablesize = lua_rawlen(L, -1);
+		if (tablesize == 5){ 
+		int* result = new int[tablesize];
+		printf("tablesize: %d\n",tablesize );
+		for (int i = 1; i <= tablesize; i++) {
+			lua_pushinteger(L, i); 
+			lua_gettable(L, -2);
+			result[i-1] = lua_tointeger(L, -1); //I only need ints here, otherwise I'd need to check the type
+			printf("V: %d\n", result[i-1]);
+			lua_pop(L, 1);
+		}
+		super(result[0], result[1], result[2], result[3], result[4]);
+		return;
+		}
+	}
+	super(type, charge, firsttime, slot, vardata);
+}
+//AddCollectible Callback (id: 1004 enum pending)
