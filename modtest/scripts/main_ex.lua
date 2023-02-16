31f44d1462ad268be891fe810ac66aa7896900d7
Meowlala <Meowlala@users.noreply.github.com>
2023-02-16 12:29:11 -0600

    type checking for all base game callbacks

--- modtest/scripts/main_ex.lua
+++ modtest/scripts/main_ex.lua
@@ -13,22 +13,117 @@ for k, v in pairs(ModCallbacks) do
 	callbackIDToName[v] = k
 end
 
-local typecheckFunctions = {}
-
-typecheckFunctions[ModCallbacks.MC_PRE_ADD_COLLECTIBLE] = function(ret) --TODO might be better as a table of paths to take depending on ret type? might help performance a bit, less if/else statements
-	if type(ret) == "table" then
-		local tablesize = #ret
-		if tablesize ~= 5 then
-			return "bad return table length (5 expected, got " .. tostring(tablesize) .. ")"
-		end
-	elseif type(ret) == "number" and math.type(ret) == "integer" then -- valid
-	else
-		return "bad return type (table or integer expected, got " .. tostring(type(ret)) .. ")"
+local function checkInteger(val)
+	if math.type(val) ~= "integer" then
+		return "bad return type (number has no integer representation)"
 	end
 end
 
-rawset(Isaac, "SetCallbackTypeCheck", function(callbackID, func)
-	typecheckFunctions[callbackID] = func
+local function checkTableSizeFunction(size)
+	return function(val)
+		local tablesize = #val
+		if tablesize ~= size then
+			return "bad return table length (" .. tostring(size) .. " expected, got " .. tostring(tablesize) .. ")"
+		end
+	end
+end
+
+local function backEnum(enum, id)
+	for k, v in pairs(enum) do
+		if v == id then
+			return k
+		end
+	end
+end
+
+local typecheckFunctions = {
+	[ModCallbacks.MC_PRE_ADD_COLLECTIBLE] = {
+		["table"] = checkTableSizeFunction(5),
+		["number"] = checkInteger
+	},
+	[ModCallbacks.MC_INPUT_ACTION] = {
+		["number"] = function(ret, retType, entity, hook)
+			if hook ~= InputHook.GET_ACTION_VALUE then
+				return "bad return type for hook " .. backEnum(InputHook, hook) .. " (boolean expected, got number)"
+			elseif ret < 0 or ret > 1 then
+				return "bad return value for hook GET_ACTION_VALUE (number must be within range 0-1)"
+			end
+		end,
+		["boolean"] = function(ret, retType, entity, hook)
+			if hook == InputHook.GET_ACTION_VALUE then
+				return "bad return type for hook GET_ACTION_VALUE (number expected, got boolean)"
+			end
+		end
+	},
+	[ModCallbacks.MC_EXECUTE_CMD] = {}, -- returning any value causes the game to crash
+	[ModCallbacks.MC_PRE_ENTITY_SPAWN] = {
+		["table"] = checkTableSizeFunction(4)
+	},
+	[ModCallbacks.MC_POST_PICKUP_SELECTION] = {
+		["table"] = checkTableSizeFunction(2)
+	},
+	[ModCallbacks.MC_PRE_ROOM_ENTITY_SPAWN] = {
+		["table"] = checkTableSizeFunction(3)
+	},
+}
+
+local boolCallbacks = {
+	ModCallbacks.MC_PRE_USE_ITEM,
+	ModCallbacks.MC_ENTITY_TAKE_DMG,
+	ModCallbacks.MC_PRE_FAMILIAR_COLLISION,
+	ModCallbacks.MC_PRE_PICKUP_COLLISION,
+	ModCallbacks.MC_PRE_TEAR_COLLISION,
+	ModCallbacks.MC_PRE_PROJECTILE_COLLISION,
+	ModCallbacks.MC_PRE_BOMB_COLLISION,
+	ModCallbacks.MC_PRE_KNIFE_COLLISION,
+	ModCallbacks.MC_PRE_PLAYER_COLLISION,
+	ModCallbacks.MC_PRE_NPC_COLLISION,
+	ModCallbacks.MC_PRE_LASER_COLLISION,
+	ModCallbacks.MC_PRE_NPC_UPDATE,
+	ModCallbacks.MC_PRE_ENTITY_DEVOLVE,
+}
+
+for _, callback in ipairs(boolCallbacks) do
+	typecheckFunctions[callback] = {["boolean"] = true}
+end
+
+local intCallbacks = {
+	ModCallbacks.MC_POST_CURSE_EVAL,
+	ModCallbacks.MC_GET_CARD,
+	ModCallbacks.MC_PRE_GET_COLLECTIBLE,
+	ModCallbacks.MC_GET_PILL_COLOR,
+	ModCallbacks.MC_GET_PILL_EFFECT,
+	ModCallbacks.MC_GET_TRINKET,	
+}
+
+for _, callback in ipairs(intCallbacks) do
+	typecheckFunctions[callback] = {["number"] = checkInteger}
+end
+
+for callback, typeTable in pairs(typecheckFunctions) do
+	local validTypes = {}
+	for typ, func in pairs(typeTable) do
+		validTypes[#validTypes + 1] = typ
+	end
+	
+	table.sort(validTypes)
+	
+	local expected = ""
+	for i, typ in ipairs(validTypes) do
+		if i ~= #validTypes then
+			expected = expected .. typ .. ", "
+		elseif #validTypes == 1 then
+			expected = expected .. typ
+		else
+			expected = expected .. "or " .. typ
+		end
+	end
+	
+	typeTable.expectedtypes = expected
+end
+
+rawset(Isaac, "SetCallbackTypeCheck", function(callbackID, tbl)
+	typecheckFunctions[callbackID] = tbl
 end)
 
 rawset(Isaac, "SetCallbackMatchTest", function(callbackID, func)
@@ -77,8 +172,16 @@ function _RunCallback(callbackID, Param, ...)
 				if status then
 					if ret ~= nil then
 						local err
-						if typecheckFunctions[callbackID] then
-							err = typecheckFunctions[callbackID](ret)
+						local typeCheck = typecheckFunctions[callbackID]
+						if typeCheck then
+							local typ = type(ret)
+							if typeCheck[typ] == true then
+							elseif typeCheck[typ] then
+								err = typeCheck[typ](ret, typ, ...)
+							else
+								err = "bad return type (" .. typeCheck.expectedtypes .. " expected, got " .. tostring(typ) .. ")"
+							end
+							
 							if err then
 								local info = debug_getinfo(callback.Function, "S")
 								logError(callbackID, callback.Mod.Name, info.short_src .. ": " .. info.linedefined .. ": " .. err)
