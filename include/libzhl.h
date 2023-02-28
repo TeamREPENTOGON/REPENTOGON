05312da18e5710089f9ec705486789343a9016b7
Sylmir <sylmir.dev@gmail.com>
2023-02-28 20:45:24 +0100

    Remove LIBZHL_GLOB

--- include/libzhl.h
+++ include/libzhl.h
@@ -1,10 +1,8 @@
 
 #if defined(libzhl_EXPORTS)
 #define LIBZHL_API __declspec(dllexport)
-#define LIBZHL_API_GLOBS __declspec(dllexport)
 #else
 #define LIBZHL_API __declspec(dllimport)
-#define LIBZHL_API_GLOBS __declspec(dllimport)
 #endif
 
 #define MOD_EXPORT extern "C" __declspec(dllexport)
