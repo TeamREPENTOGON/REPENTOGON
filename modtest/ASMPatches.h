65600094f96ac402d547f20e8a5a904e16b2d68f
Jeremy Silliman <jsilliman@protonmail.com>
2023-05-14 15:24:35 -0400

    port ambush ASM patch to ASMPatch framework

--- modtest/ASMPatches.h
+++ modtest/ASMPatches.h
@@ -13,148 +13,9 @@ static DWORD SetPageMemoryRW(void* addr, MEMORY_BASIC_INFORMATION* info) {
 	return old_protect;
 }
 
-//TODO port these patches to ASMPatcher
-
-// Much of this is adapted from Sylmir's WIP Delirium patches, just adapted for use with the LibZHL sigscanner.
-// Thanks Sylmir <3
 extern int ambushWaves;
 extern bool overrideMegaSatanEnding;
 extern void __stdcall LogMessageCallback(const char* logMessage);
-
-void PatchHardcodedAmbushWaveCount() {
-	printf("[REPENTOGON] ASM patch: hardcoded wave count\n");
-
-
-	// This signature takes us straight to the small block that calculates how many ambush waves should happen.
-	SigScan scanner("33db83f801");
-	void* addr;
-	bool result = scanner.Scan();
-	if (!result) {
-		throw std::runtime_error("Failed to find address!\n");
-	}
-	else {
-		addr = scanner.GetAddress();
-		printf("Found address for hardcoded challenge waves: %p\n", addr);
-	}
-
-	// First, we allocate memory for our new ASM to live in
-	// 0x17 is the length of our code block below
-	void* region = VirtualAlloc(NULL, 0x17, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (region == NULL) {
-		throw std::runtime_error("Unable to allocate memory\n");
-	}
-
-	printf("Allocated memory for PatchHardcodedAmbushWaveCount at %p\n", region);
-
-	// Set permissions on memory
-	MEMORY_BASIC_INFORMATION info;
-	DWORD old_protect = SetPageMemoryRW(addr, &info);
-
-	/* This is the assembly code which will live in our new memory region, manually denoted in hexadecimal format.
-	   We piggyback off of the game code already setting up the boss room check for us in the EAX register-
-		 we simply execute the comparison, and act accordingly.
-	   If the comparison matches, move 2 to the EBX register, where the wave count temporarily lives.
-	   Otherwise, move a pointer to ambushWaves to the EBX register.
-
-	   We create a skeleton, with placeholder values (denoted as XXXXXXXX in the comments).
-	   These values are calculated, filled, and copied to the memory block we allocate.
-	 */
-
-	char _code[] = {
-		// cmp eax, 1 (Check against the pre-set comparison.)
-		0x83, 0xF8, 0x01, // 0x3
-		// jne XXXXXXXX (If we're not a boss challenge room, jump 0x7 bytes ahead.)
-		0x75, 0x07, // 0x5
-		// mov ebx, 2 (Move the value 2 to the EBX register.)
-		0xBB, 0X02, 0, 0, 0, // 0xA
-		// jmp isaac-ng.XXXXXXXX (Jump 0x6 bytes ahead)
-		0xEB, 0x06, // 0xC
-		// mov ebx, dword ptr ds:[0xXXXXXXXX] (Move the value stored in our ambushWaves pointer to the EBX register.)
-		0x8B, 0x1D, 0, 0, 0, 0, // 0x12
-		// jmp isaac-ng.XXXXXXXX (Jump to the original code.)
-		0xE9, 0, 0, 0, 0 // 0x17
-	};
-
-	// Create a pointer to ambushWaves, so we can dynamically update it in Lua
-	void* ptr = &ambushWaves;
-
-	// Allocate new memory, and copy code to our block.
-	char* code = (char*)malloc(sizeof(_code));
-	memcpy(code, _code, 0x17);
-
-	// Copy the pointer address to the proper position in the block.
-	memcpy(code + 0x12 - 4, &ptr, sizeof(ptr));
-
-	// Copy the JMP address to the proper position in the block.
-	// We need to jump 0xB past our original block to bypass the vanilla calculation properly.
-	void* offset = (char*)addr + 0xB;
-	ptrdiff_t jmp = (char*)offset - ((char*)region + 0x17);
-
-	memcpy(code + 0x17 - 4, &jmp, sizeof(jmp));
-
-	// Finally, copy the resulting code to our allocated memory.
-	memcpy(region, code, 0x1C);
-
-
-	MEMORY_BASIC_INFORMATION new_region_info;
-	VirtualQuery(region, &new_region_info, sizeof(new_region_info));
-	DWORD _dummy;
-	VirtualProtect(new_region_info.BaseAddress, new_region_info.RegionSize, PAGE_EXECUTE_READ, &_dummy);
-
-	// At the original address, override the instruction with a JMP to our block.
-	char override_base[] = {
-		0xe9, 0x00, 0x00, 0x00, 0x00
-	};
-
-	ptrdiff_t jump_dist = (char*)region - ((char*)addr + 0x5);
-	memcpy(override_base + 1, &jump_dist, sizeof(jump_dist));
-	memcpy(addr, override_base, 5);
-
-	VirtualProtect(info.BaseAddress, info.RegionSize, old_protect, &_dummy);
-
-	FlushInstructionCache(GetModuleHandle(NULL), NULL, 0);
-}
-
-void* PerformInputActionMenuPatch(const char* signature) {
-	SigScan scanner(signature);
-	void* addr;
-	if (!scanner.Scan()) {
-		throw std::runtime_error("Failed to find address!\n");
-	}
-	else {
-		addr = scanner.GetAddress();
-	}
-
-
-	MEMORY_BASIC_INFORMATION info;
-	DWORD old_protect = SetPageMemoryRW(addr, &info);
-	DWORD _dummy;
-
-	char override_base[] = {
-		// jmp isaac-ng.XXXXXXXX
-		0x90, 0x90, 0x90, 0x90, 0x90, 0x90
-	};
-	memcpy(addr, override_base, 6);
-
-
-	VirtualProtect(info.BaseAddress, info.RegionSize, old_protect, &_dummy);
-
-	FlushInstructionCache(GetModuleHandle(NULL), NULL, 0);
-
-	return addr;
-}
-
-// INPUT_ACTION is called in four separate places, and the check is duplicated and identical across all four.
-// In all four cases, we can just replace the check with six NOPs to remove the check.
-// This will allow mods to check button presses on the main menu again, like in AB+.
-void PatchInputActionMenuCheck() {
-	printf("[REPENTOGON] ASM patch: MC_INPUT_ACTION on main menu\n");
-	printf("Found all four main menu patch addresses: %p, %p, %p, %p\n",
-		PerformInputActionMenuPatch("837f??0275??833d????????0074??8d45??505351"),
-		PerformInputActionMenuPatch("8378??0275??833d????????00"),
-		PerformInputActionMenuPatch("837f??0275??833d????????0074??8d45??50536a00"),
-		PerformInputActionMenuPatch("837f??0275??833d????????0074??8d45??50536a01"));
-}
 
 /* Patch 1
  Replace the RNG::RandomInt call with a JMP to our own code.
@@ -248,39 +109,68 @@ void PatchHardcodedMegaSatanGameEnding() {
 	FlushInstructionCache(GetModuleHandle(NULL), NULL, 0);
 }
 
-/* This patch hooks KAGE_LogMessage by hand.LibZHL can't properly hook functions with varargs, and we need varargs to properly get log messages.
+/* This patch hooks KAGE_LogMessage by hand. LibZHL can't properly hook functions with varargs, and we need varargs to properly get log messages.
    So, we'll just do it manually, not a big deal.
    We manually call a trampoline function that takes a const char* as an input, and prints it to our ImGui log.
    At the point we hook, KAGE_LogMessage has already fully processed its varargs, and is storing the result in the ECX register.
+   We pass ECX to our trampoline as a result.
    */
-
 void ASMPatchLogMessage() {
-	SigScan scanner("8d51??8a014184c075??2bca80b9????????0a"); // KAGE_LogMessage
+	SigScan scanner("8d51??8a014184c075??2bca80b9????????0a");
 	scanner.Scan();
 	void* addr = scanner.GetAddress();
 
 	ASMPatch patch;
-	patch.AddBytes("\x51"); // Push ECX to the stack, so we can restore it as the function expects later
-	patch.AddBytes("\x51"); // Push ECX one more time, for use on our function (this will be consumed).
-	patch.AddInternalCall(LogMessageCallback);
-	patch.AddBytes("\x59"); // Pop ECX
+	patch.AddBytes("\x51") // Push ECX to the stack, so we can restore it as the function expects later
+	    .AddBytes("\x51") // Push ECX one more time, for use on our function (this will be consumed).
+	    .AddInternalCall(LogMessageCallback)
+	    .AddBytes("\x59") // Pop ECX
 
 	// Reintroduce overwritten assembly + some extra to work around some broken jumps
-	patch.AddBytes("\x8D\x51\x01"); // lea edx,dword pointer ds:[ecx+1]
-	patch.AddBytes("\x8A\x01"); // mov al,byte ptr ds:[ecx]
-	patch.AddBytes("\x41"); // inc ecx
-	patch.AddBytes("\x84\xC0"); // test al, al
-	patch.AddBytes("\x75\xF9"); // jne xxxxxxxx
-	patch.AddRelativeJump((char*)addr + 0x11);
+	    .AddBytes("\x8D\x51\x01") // lea edx,dword pointer ds:[ecx+1]
+	    .AddBytes("\x8A\x01") // mov al,byte ptr ds:[ecx]
+	    .AddBytes("\x41") // inc ecx
+	    .AddBytes("\x84\xC0") // test al, al
+	    .AddBytes("\x75\xF9") // jne xxxxxxxx
+	    .AddRelativeJump((char*)addr + 0x11);
+	sASMPatcher.PatchAt(addr, &patch);
+}
+
+/* Ambush waves have a hardcoded amount. This patch works around it by feeding the game a pointer to an int we control instead of the hardcoded 3.
+   Boss rooms can't be changed in this manner as they have more setup.
+   The game already sets up a check to see if we're in a boss ambush room in the EAX register at the point we hook.
+   We compare against the pre-set check. If we're a boss room, move 2 to the EBX register, otherwise, move our pointer to the EBX register.
+*/
+void ASMPatchAmbushWaveCount() {
+	SigScan scanner("33db83f801");
+	scanner.Scan();
+	void* addr = scanner.GetAddress();
+
+	printf("[REPENTOGON] Patching hardcoded ambush wave count at %p\n", addr);
+
+	char ptrMov[] = {
+		0x8B, 0X1D, 0, 0, 0, 0
+	};
+
+	void* ptr = &ambushWaves;
+	memcpy(ptrMov + 2, &ptr, sizeof(ptr));
+
+	ASMPatch patch;
+	patch.AddBytes("\x83\xF8\x01") // cmp eax, 1
+		.AddBytes("\x75\x07") // jne (current addr + 0x7)
+		.AddBytes("\xBB\x02").AddZeroes(3) // mov ebx, 2
+        .AddBytes("\xEB\x06") // jmp (current addr + 0x6);
+		.AddBytes(ptrMov) // mov ebx, dword ptr ds:[0xXXXXXXXX]
+		.AddRelativeJump((char*)addr + 0xB);
 	sASMPatcher.PatchAt(addr, &patch);
 }
 
 static void PerformASMPatches() {
 	// OLD SCHOOL GAMER
-	PatchHardcodedAmbushWaveCount();
 	PatchHardcodedMegaSatanGameEnding();
 	//PatchInputActionMenuCheck();
 
 	// NEW SCHOOL GAMER
 	ASMPatchLogMessage();
+	ASMPatchAmbushWaveCount();
 }
