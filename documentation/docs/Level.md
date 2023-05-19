0d6db3cd2234ead5b98cd88f9678fc19272996cb
kittenchilly <mario.pikmin.zelda@gmail.com>
2023-05-19 12:11:11 -0500

    alt path puzzles

--- documentation/docs/Level.md
+++ documentation/docs/Level.md
@@ -4,6 +4,20 @@
 
 ### Get·Dimension () {: aria-label='Functions' }
 #### [Dimension](enums/Dimension.md) GetDimension ( ) {: .copyable aria-label='Functions' }
+___
+### Has·Abandoned·Mineshaft () {: aria-label='Functions' }
+#### boolean HasAbandonedMineshaft ( ) {: .copyable aria-label='Functions' }
+Returns `true` if the floor has the mineshaft room used for the second Knife Piece puzzle.
+
+___
+### Has·Mirror·Dimension () {: aria-label='Functions' }
+#### boolean HasMirrorDimension ( ) {: .copyable aria-label='Functions' }
+Returns `true` if the floor has the mirror dimension used for the first Knife Piece puzzle.
+
+___
+### Has·Photo·Door () {: aria-label='Functions' }
+#### boolean HasPhotoDoor ( ) {: .copyable aria-label='Functions' }
+Returns `true` if the floor has the photo door used to enter Mausoleum/Gehenna leading to the Ascent sequence.
 
 ___
 ### Place·Room () {: aria-label='Functions' }
--- modtest/LuaLevel.cpp
+++ modtest/LuaLevel.cpp
@@ -12,6 +12,30 @@ static int Lua_LevelCanSpawnDoorOutline(lua_State* L) {
 	return 1;
 }
 
+int Lua_LevelHasAbandonedMineshaft(lua_State* L)
+{
+	Level* level = lua::GetUserdata<Level*>(L, 1, lua::Metatables::LEVEL, "Level");
+	lua_pushboolean(L, level->HasAbandonedMineshaft());
+
+	return 1;
+}
+
+int Lua_LevelHasMirrorDimension(lua_State* L)
+{
+	Level* level = lua::GetUserdata<Level*>(L, 1, lua::Metatables::LEVEL, "Level");
+	lua_pushboolean(L, level->HasMirrorDimension());
+
+	return 1;
+}
+
+int Lua_LevelHasPhotoDoor(lua_State* L)
+{
+	Level* level = lua::GetUserdata<Level*>(L, 1, lua::Metatables::LEVEL, "Level");
+	lua_pushboolean(L, level->HasPhotoDoor());
+
+	return 1;
+}
+
 HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
 	super();
 	lua_State* state = g_LuaEngine->_state;
@@ -19,4 +43,7 @@ HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
 	lua::Metatables mt = lua::Metatables::LEVEL;
 
 	lua::RegisterFunction(state, mt, "CanSpawnDoorOutline", Lua_LevelCanSpawnDoorOutline);
+	lua::RegisterFunction(state, mt, "HasAbandonedMineshaft", Lua_LevelHasAbandonedMineshaft);
+	lua::RegisterFunction(state, mt, "HasMirrorDimension", Lua_LevelHasMirrorDimension);
+	lua::RegisterFunction(state, mt, "HasPhotoDoor", Lua_LevelHasPhotoDoor);
 }
