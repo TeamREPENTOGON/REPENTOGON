575b514be392017c1427416468ec877023a95a47
Yaroslav <36567300+epfly6@users.noreply.github.com>
2023-06-08 23:54:50 +0300

    Lazer Colleciton
    --Added float Laser:GetScale()
    --Added void Laser:SetScale( float Scale)
    --Added void Laser:ResetSpriteScale()

---
+++ modtest/LuaEntityLaser.cpp
@@ -0,0 +1,40 @@
+#include <lua.hpp>
+
+#include "IsaacRepentance.h"
+#include "LuaCore.h"
+#include "HookSystem.h"
+
+static int Lua_EntityLaserGetScale(lua_State* L)
+{
+	Entity_Laser* laser = lua::GetUserdata<Entity_Laser*>(L, 1, lua::Metatables::ENTITY, "EntityLaser");
+	lua_pushnumber(L, *laser->GetScale());
+
+	return 1;
+}
+
+static int Lua_EntityLaserSetScale(lua_State* L)
+{
+	Entity_Laser* laser = lua::GetUserdata<Entity_Laser*>(L, 1, lua::Metatables::ENTITY, "EntityLaser");
+	*laser->GetScale() = luaL_checknumber(L, 2);
+	laser->ResetSpriteScale();
+
+	return 0;
+}
+
+static int Lua_EntityLaserResetSpriteScale(lua_State* L)
+{
+	Entity_Laser* laser = lua::GetUserdata<Entity_Laser*>(L, 1, lua::Metatables::ENTITY, "EntityLaser");
+	laser->ResetSpriteScale();
+
+	return 0;
+}
+
+HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
+	super();
+	lua_State* state = g_LuaEngine->_state;
+	lua::LuaStackProtector protector(state);
+	lua::Metatables mt = lua::Metatables::ENTITY_LASER;
+	lua::RegisterFunction(state, mt, "GetScale", Lua_EntityLaserGetScale);
+	lua::RegisterFunction(state, mt, "SetScale", Lua_EntityLaserSetScale);
+	lua::RegisterFunction(state, mt, "ResetSpriteScale", Lua_EntityLaserResetSpriteScale);
+}
