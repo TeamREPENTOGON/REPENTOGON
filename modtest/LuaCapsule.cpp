726086033f2a1fb18b7ea2fe9f8c972e70e65a2d
Yaroslav <36567300+epfly6@users.noreply.github.com>
2023-06-08 21:14:45 +0300

    capsus

--- modtest/LuaCapsule.cpp
+++ modtest/LuaCapsule.cpp
@@ -9,8 +9,7 @@ static constexpr const char* CapsuleMT = "Capsule";
 static int Lua_EntityGetNullCapsule(lua_State* L) {
 	Entity* ent = lua::GetUserdata<Entity*>(L, 1, lua::Metatables::ENTITY, "Entity");
 	const char* str = luaL_checkstring(L, 2);
-	Capsule res;
-	res = *ent->GetNullCapsule(&res, str);
+	Capsule& res = *ent->GetNullCapsule(nullptr, str);
 
 	Capsule** ud = (Capsule**)lua_newuserdata(L, sizeof(Capsule*));
 	*ud = &res;
@@ -20,12 +19,10 @@ static int Lua_EntityGetNullCapsule(lua_State* L) {
 
 static int Lua_EntityGetCollisionCapsule(lua_State* L) {
 	Entity* ent = lua::GetUserdata<Entity*>(L, 1, lua::Metatables::ENTITY, "Entity");
-	const Vector* vec = lua::GetUserdata<Vector*>(L, 2, lua::Metatables::VECTOR, "Vector");
-	Capsule res;
-	res = *ent->GetCollisionCapsule(&res, *vec);
+	Vector* vec = lua::GetUserdata<Vector*>(L, 2, lua::Metatables::VECTOR, "Vector");
 
 	Capsule* ud = (Capsule*)lua_newuserdata(L, sizeof(Capsule));
-	*ud = res; //lol
+	*ud = ent->GetCollisionCapsule(vec);
 	luaL_setmetatable(L, CapsuleMT);
 	return 1;
 };
@@ -38,15 +35,6 @@ static int Lua_CapsuleGetVec1(lua_State* L)
 
 	return 1;
 }
-
-static int Lua_CapsuleGetSus(lua_State* L)
-{
-	Capsule* capsule = *lua::GetUserdata<Capsule**>(L, 1, CapsuleMT);
-	lua_pushnumber(L, *capsule->GetSus());
-
-	return 1;
-}
-
 static void RegisterCapsule(lua_State* L) {
 
 	luaL_newmetatable(L, CapsuleMT);
@@ -57,7 +45,6 @@ static void RegisterCapsule(lua_State* L) {
 	luaL_Reg functions[] = {
 		{ "GetVec1", Lua_CapsuleGetVec1 },
 		//{ "GetVec2", Lua_AmbushStartChallenge },
-		{ "GetVar", Lua_CapsuleGetSus },
 		{ NULL, NULL }
 	};
 
