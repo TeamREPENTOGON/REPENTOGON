ba14ab700633a31c575ca0908a65bdf6c82a1bdf
Yaroslav <36567300+epfly6@users.noreply.github.com>
2023-08-14 22:57:35 +0300

    items treasure
    Added:
    - CollectibleType ItemPool:GetCollectibleFromList( table ItemList, int Seed = Random(), CollectibleType DefaultItem = CollectibleType.COLLECTIBLE_BREAKFAST, bool AddToBlacklist = false, bool ExcludeLockedItems = false )

--- documentation/docs/ItemPool.md
+++ documentation/docs/ItemPool.md
@@ -6,3 +6,7 @@
 #### [Card](https://wofsauge.github.io/IsaacDocs/rep/enums/Card.html) GetCardEx ( int Seed, int SpecialChance, int RuneChance, int SuitChance, boolean AllowNonCards ) {: .copyable aria-label='Functions' }
 More sophisticated version of [ItemPool:GetCard()](https://wofsauge.github.io/IsaacDocs/rep/ItemPool.html#getcard) that allows to define individual chances.
 ___
+### Get·Collectible·From·List () {: aria-label='Functions' }
+#### [CollectibleType](https://wofsauge.github.io/IsaacDocs/rep/enums/CollectibleType.html) GetCollectibleType ( table ItemList, int Seed = Random(), [CollectibleType](https://wofsauge.github.io/IsaacDocs/rep/enums/CollectibleType.html) DefaultItem = CollectibleType.COLLECTIBLE_BREAKFAST, boolean AddToBlacklist = false, boolean ExcludeLockedItems = false ) {: .copyable aria-label='Functions' }
+
+___
--- repentogon/LuaItemPool.cpp
+++ repentogon/LuaItemPool.cpp
@@ -16,10 +16,50 @@ int Lua_ItemPoolGetCardEx(lua_State* L)
 	return 1;
 }
 
+LUA_FUNCTION(Lua_ItemPoolGetCollectibleFromList) {
+	ItemPool* itemPool = lua::GetUserdata<ItemPool*>(L, 1, lua::Metatables::ITEM_POOL, "ItemPool");
+	if (!lua_istable(L, 2))
+	{
+		luaL_error(L, "Expected a table as second argument");
+		return 0;
+	}
+
+	size_t length = lua_rawlen(L, 2);
+
+	// if the table is empty, we should pass default item
+	if (length == 0)
+	{
+		unsigned int defaultItem = (unsigned int)luaL_optinteger(L, 4, 25); //COLLECTIBLE_BREAKFAST
+		lua_pushinteger(L, defaultItem);
+		return 1;
+	}
+
+	int* list = new int[length];
+
+	for (size_t i = 0; i < length; i++)
+	{
+		lua_rawgeti(L, 2, i + 1);
+		list[i] = (int)luaL_checkinteger(L, -1);
+		lua_pop(L, 1);
+	}
+
+	unsigned int seed = (unsigned int)luaL_optinteger(L, 3, Random());
+	unsigned int defaultItem = (unsigned int)luaL_optinteger(L, 4, 25); //COLLECTIBLE_BREAKFAST
+	bool addToBlacklist = lua_toboolean(L, 5);
+	bool excludeLockedItems = lua_toboolean(L, 6);
+	lua_pushinteger(L, itemPool->GetCollectibleFromList(list, length, seed, defaultItem, addToBlacklist, excludeLockedItems));
+
+	// delete the array
+	delete[] list;
+
+	return 1;
+}
+
 HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
 	super();
 	lua_State* state = g_LuaEngine->_state;
 	lua::LuaStackProtector protector(state);
 	lua::Metatables mt = lua::Metatables::ITEM_POOL;
 	lua::RegisterFunction(state, mt, "GetCardEx", Lua_ItemPoolGetCardEx);
+	lua::RegisterFunction(state, mt, "GetCollectibleFromList", Lua_ItemPoolGetCollectibleFromList);
 }
