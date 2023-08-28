805474a4e9ee1cca5d89a7484414da3e4d4623dc
Jeremy Silliman <jsilliman@protonmail.com>
2023-08-27 22:42:09 -0400

    remove some leftover debug text

--- repentogon/MesaShaderFix.cpp
+++ repentogon/MesaShaderFix.cpp
@@ -2,11 +2,8 @@
 #include "HookSystem.h"
 
 HOOK_STATIC(KAGE_Graphics_Manager_GL, LoadShader, (KAGE_Graphics_Shader* shader, KAGE_Graphics_VertexAttributeDescriptor* descriptor, const char* shaderPath) -> void*) {
-const char* targetShader = "shaders/coloroffset_gold";
-	// shaders/coloroffset_gold
-	printf("%s\n", shaderPath);
+	const char* targetShader = "shaders/coloroffset_gold";
 	if (strcmp(shaderPath, targetShader) == 0) {
-	    printf("shadow REALM, JIMBOy\n");
 	    shaderPath = "shaders/coloroffset_gold_mesafix";
     }
 	return super(shader, descriptor, shaderPath);
