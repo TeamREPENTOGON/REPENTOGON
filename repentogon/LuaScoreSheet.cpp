57093d66ff9b7cd0ac3b6d0f09bf20985f3f06b9
fly_6 <36567300+epfly6@users.noreply.github.com>
2023-09-08 22:39:34 +0300

    ShitScore
    Added:
    -- ScoreSheet Game:GetScoreSheet( )
    -- void ScoreSheet:Calculate( )
    -- int ScoreSheet:GetStageBonus( )
    -- int ScoreSheet:GetSchwagBonus( )
    -- int ScoreSheet:GetBlueBabyBonus( )
    -- int ScoreSheet:GetLambBonus( )
    -- int ScoreSheet:GetMegaSatanBonus( )
    -- int ScoreSheet:GetRushBonus( )
    -- int ScoreSheet:GetExplorationBonus( )
    -- int ScoreSheet:GetDamagePenalty( )
    -- int ScoreSheet:GetTimePenalty( )
    -- int ScoreSheet:GetItemPenalty( )
    -- int ScoreSheet:GetTotalScore( )
    -- int ScoreSheet:GetRunTimeLevel( )

---
+++ repentogon/LuaScoreSheet.cpp
@@ -0,0 +1,158 @@
+#include <lua.hpp>
+
+#include "IsaacRepentance.h"
+#include "LuaCore.h"
+#include "HookSystem.h"
+
+static constexpr const char* ScoreSheetMT = "ScoreSheet";
+
+static int Lua_GetScoreSheet(lua_State* L) {
+	Game* game = g_Game;
+	ScoreSheet** ud = (ScoreSheet**)lua_newuserdata(L, sizeof(ScoreSheet*));
+	*ud = game->GetScoreSheet();
+	luaL_setmetatable(L, ScoreSheetMT);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetCalculate)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	scoreSheet->Calculate();
+
+	return 0;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetStageBonus)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_stageBonus);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetSchwagBonus)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_schwagBonus);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetBlueBabyBonus)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_blueBabyBonus);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetLambBonus)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_lambBonus);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetMegaSatanBonus)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_megaSatanBonus);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetRushBonus)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_rushBonus);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetExplorationBonus)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_explorationBonus);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetDamagePenalty)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_damagePenalty);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetTimePenalty)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_timePenalty);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetItemPenalty)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_itemPenalty);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetTotalScore)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_totalScore);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_ScoreSheetGetRunTimeLevel)
+{
+	ScoreSheet* scoreSheet = *lua::GetUserdata<ScoreSheet**>(L, 1, ScoreSheetMT);
+	lua_pushinteger(L, scoreSheet->_runTimeLevel);
+
+	return 1;
+}
+
+static void RegisterScoreSheet(lua_State* L) {
+	lua::PushMetatable(L, lua::Metatables::GAME);
+	lua_pushstring(L, "GetScoreSheet");
+	lua_pushcfunction(L, Lua_GetScoreSheet);
+	lua_rawset(L, -3);
+	lua_pop(L, 1);
+
+	luaL_newmetatable(L, ScoreSheetMT);
+	lua_pushstring(L, "__index");
+	lua_pushvalue(L, -2);
+	lua_settable(L, -3);
+
+	luaL_Reg functions[] = {
+		{ "Calculate", Lua_ScoreSheetCalculate },
+		{ "GetStageBonus", Lua_ScoreSheetGetStageBonus },
+		{ "GetSchwagBonus", Lua_ScoreSheetGetSchwagBonus },
+		{ "GetBlueBabyBonus", Lua_ScoreSheetGetBlueBabyBonus },
+		{ "GetLambBonus", Lua_ScoreSheetGetLambBonus },
+		{ "GetMegaSatanBonus", Lua_ScoreSheetGetMegaSatanBonus },
+		{ "GetRushBonus", Lua_ScoreSheetGetRushBonus },
+		{ "GetExplorationBonus", Lua_ScoreSheetGetExplorationBonus },
+		{ "GetDamagePenalty", Lua_ScoreSheetGetDamagePenalty },
+		{ "GetTimePenalty", Lua_ScoreSheetGetTimePenalty },
+		{ "GetItemPenalty", Lua_ScoreSheetGetItemPenalty },
+		{ "GetTotalScore", Lua_ScoreSheetGetTotalScore },
+		{ "GetRunTimeLevel", Lua_ScoreSheetGetRunTimeLevel },
+		{ NULL, NULL }
+	};
+
+	luaL_setfuncs(L, functions, 0);
+	lua_pop(L, 1);
+
+}
+
+HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
+	super();
+	RegisterScoreSheet(_state);
+}
