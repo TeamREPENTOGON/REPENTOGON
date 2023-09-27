83a3d1dec04c9bd8d96d362013971cf662e2c98e
Sylmir <sylmir.dev@gmail.com>
2023-09-27 22:15:54 +0200

    Debug.ListLoadedFiles, Debug.ForceUnload

--- repentogon/LuaDebug.cpp
+++ repentogon/LuaDebug.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
 
+#include <regex>
+
 #include <lua.hpp>
 
 #include "HookSystem.h"
@@ -26,13 +28,72 @@ namespace Debug {
 			return 1;
 		}
 	}
+
+	LUA_FUNCTION(ListLoadedFiles) {
+		int i = 1;
+		lua_settop(L, 0);
+		lua_newtable(L); // Index 1
+		lua_getfield(L, LUA_REGISTRYINDEX, LUA_LOADED_TABLE); // Index 2
+		lua_pushnil(L);
+		while (lua_next(L, 2) != 0) {
+			lua_pushinteger(L, i++); // Key in new table
+			lua_pushvalue(L, -3); // Value (key in the LOADED table)
+			lua_rawset(L, 1); // Set in new table. 
+			lua_pop(L, 1); // Pop the value in the LOADED table (only the key in LOADED remains)
+		}
+		lua_pop(L, 1); // Pop LOADED
+		return 1;
+	}
+
+	LUA_FUNCTION(ForceUnload) {
+		const char* base = luaL_checkstring(L, 1);
+		std::vector<std::string> modules;
+		std::regex r("/");
+
+		// Retrieve all module names, replacing the forward slashes in them with dots
+		int loadedIndex = 2, resultIndex = 1;
+		int i = 1;
+		lua_settop(L, 0);
+		lua_newtable(L);
+		lua_getfield(L, LUA_REGISTRYINDEX, LUA_LOADED_TABLE); // Index 2
+
+
+		lua_pushnil(L);
+		while (lua_next(L, loadedIndex) != 0) {
+			const char* name = lua_tostring(L, -2);
+			modules.push_back(std::regex_replace(name, r, std::string(".")));
+			lua_pop(L, 1);
+		}
+
+		// Locate all modules that match the input parameter
+		auto start = base;
+		size_t len = strlen(base);
+		auto end = base + len;
+		for (std::string& s : modules) {
+			if (std::equal(start, end, s.begin())) {
+				// lua_getfield(L, 1, s.c_str());
+				lua_pushnil(L);
+				lua_setfield(L, loadedIndex, s.c_str());
+				lua_pushinteger(L, i++);
+				lua_pushstring(L, s.c_str());
+				lua_rawset(L, resultIndex);
+			}
+		}
+
+		lua_pop(L, 1);
+		return 1;
+	}
 }
 
 static void RegisterDebugMethods(lua_State* L) {
 	lua_newtable(L);
-	lua_pushstring(L, "GetSignature");
-	lua_pushcfunction(L, Debug::GetSignature);
-	lua_rawset(L, -3);
+	luaL_Reg functions[] = {
+		{ "GetSignature", Debug::GetSignature },
+		{ "ListLoadedFiles", Debug::ListLoadedFiles },
+		{ "ForceUnload", Debug::ForceUnload },
+		{ NULL, NULL }
+	};
+	luaL_setfuncs(L, functions, 0);
 	lua_setglobal(L, "Debug");
 }
 
