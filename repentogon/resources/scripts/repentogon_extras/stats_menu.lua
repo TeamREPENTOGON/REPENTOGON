c51fb85da79f34d3c5efa8dcb74b62a42e762197
Wofsauge <jan-.-@t-online.de>
2023-10-04 18:36:09 +0200

    Made it harder for mods to break repentogon extra scripts

--- repentogon/resources/scripts/repentogon_extras/stats_menu.lua
+++ repentogon/resources/scripts/repentogon_extras/stats_menu.lua
@@ -1,3 +1,12 @@
+-- save copy of original enums, to make it harder for mods to mess with the repentogon features
+local _Achievement = Achievement
+local _EventCounter = EventCounter
+local _ModCallbacks = ModCallbacks
+local _CollectibleType = CollectibleType
+local _MainMenuType = MainMenuType
+local _ButtonAction = ButtonAction
+local _SoundEffect = SoundEffect 
+
 local StatsMenuRep = {
     StatPageAngleOffsetX = -2,
     StatPageNumberOffsetX = 120,
@@ -12,13 +21,13 @@ local StatsMenuRep = {
     Stats = {
         {
             "GAME STATS", {
-            { "deaths",    EventCounter.DEATHS },
+            { "deaths", _EventCounter.DEATHS },
             { "items", function()
                 local gameData = Isaac.GetPersistentGameData()
                 local itemconfig = Isaac.GetItemConfig()
                 local sum = 0
                 local unlocked = 0
-                for id = 1, CollectibleType.NUM_COLLECTIBLES - 1 do
+                for id = 1, _CollectibleType.NUM_COLLECTIBLES - 1 do
                     local configEntry = itemconfig:GetCollectible(id)
                     if configEntry and not configEntry.Hidden then
                         sum = sum + 1
@@ -30,13 +39,13 @@ local StatsMenuRep = {
 
                 return unlocked .. "/" .. sum
             end },
-            { "mom kills", EventCounter.MOM_KILLS },
+            { "mom kills", _EventCounter.MOM_KILLS },
             { "secrets", function()
                 local gameData = Isaac.GetPersistentGameData()
                 local sum = 0
                 local unlocked = 0
                 local previousValues = {}
-                for _, value in pairs(Achievement) do
+                for _, value in pairs(_Achievement) do
                     if not previousValues[value] then
                         sum = sum + 1
                         previousValues[value] = true
@@ -48,63 +57,63 @@ local StatsMenuRep = {
 
                 return unlocked .. "/" .. sum
             end },
-            { "best streak",   EventCounter.BEST_STREAK },
+            { "best streak",   _EventCounter.BEST_STREAK },
             { "",              function() return "" end }, -- spacer
-            { "eden tokens",   EventCounter.EDEN_TOKENS },
+            { "eden tokens",   _EventCounter.EDEN_TOKENS },
             { "",              function() return "" end }, -- spacer
-            { "dailys played", EventCounter.DAILYS_PLAYED },
-            { "dailys won",    EventCounter.DAILYS_WON },
+            { "dailys played", _EventCounter.DAILYS_PLAYED },
+            { "dailys won",    _EventCounter.DAILYS_WON },
         } },
         {
             "BOSS KILLS", {
-            { "mom kills",          EventCounter.MOM_KILLS },
-            { "isaac kills",        EventCounter.ISAAC_KILLS },
-            { "satan kills",        EventCounter.SATAN_KILLS },
-            { "blue baby kills",    EventCounter.BLUE_BABY_KILLS },
-            { "lamb kills",         EventCounter.LAMB_KILLS },
-            { "hush kills",         EventCounter.HUSH_KILLS },
-            { "delirium kills",     EventCounter.DELIRIUM_KILLS },
-            { "mega satan kills",   EventCounter.MEGA_SATAN_KILLS },
-            { "mother kills",       EventCounter.MOTHER_KILLS },
-            { "beast kills",        EventCounter.BEAST_KILLS },
-            { "baby plum kills",    EventCounter.BABY_PLUM_KILLS },
-            { "boss rushs cleared", EventCounter.BOSSRUSHS_CLEARED },
+            { "mom kills",          _EventCounter.MOM_KILLS },
+            { "isaac kills",        _EventCounter.ISAAC_KILLS },
+            { "satan kills",        _EventCounter.SATAN_KILLS },
+            { "blue baby kills",    _EventCounter.BLUE_BABY_KILLS },
+            { "lamb kills",         _EventCounter.LAMB_KILLS },
+            { "hush kills",         _EventCounter.HUSH_KILLS },
+            { "delirium kills",     _EventCounter.DELIRIUM_KILLS },
+            { "mega satan kills",   _EventCounter.MEGA_SATAN_KILLS },
+            { "mother kills",       _EventCounter.MOTHER_KILLS },
+            { "beast kills",        _EventCounter.BEAST_KILLS },
+            { "baby plum kills",    _EventCounter.BABY_PLUM_KILLS },
+            { "boss rushs cleared", _EventCounter.BOSSRUSHS_CLEARED },
         } },
         {
             "SHOPPING", {
-            { "shopkeeper killed",                   EventCounter.SHOPKEEPER_KILLED },
-            { "items bought",                        EventCounter.SHOP_ITEMS_BOUGHT },
-            { "angel deals taken",                   EventCounter.ANGEL_DEALS_TAKEN },
-            { "devil deals taken",                   EventCounter.DEVIL_DEALS_TAKEN },
+            { "shopkeeper killed",                   _EventCounter.SHOPKEEPER_KILLED },
+            { "items bought",                        _EventCounter.SHOP_ITEMS_BOUGHT },
+            { "angel deals taken",                   _EventCounter.ANGEL_DEALS_TAKEN },
+            { "devil deals taken",                   _EventCounter.DEVIL_DEALS_TAKEN },
             { "",                                    function() return "" end }, -- spacer
-            { { "donation machine", "coins" },       EventCounter.DONATION_MACHINE_COUNTER },
-            { { "greed donation", "machine coins" }, EventCounter.GREED_DONATION_MACHINE_COUNTER },
+            { { "donation machine", "coins" },       _EventCounter.DONATION_MACHINE_COUNTER },
+            { { "greed donation", "machine coins" }, _EventCounter.GREED_DONATION_MACHINE_COUNTER },
         } },
         {
             "GAMBLING", {
-            { "arcades entered",                    EventCounter.ARCADES_ENTERED },
-            { "shellgames played",                  EventCounter.SHELLGAMES_PLAYED },
-            { { "blood donation", "machine used" }, EventCounter.BLOOD_DONATION_MACHINE_USED },
-            { { "slot machines", "broken" },        EventCounter.SLOT_MACHINES_BROKEN },
-            { { "golden chests", "opened" },        EventCounter.CHESTS_OPENED_WITH_KEY },
-            { { "secret room", "walls broken" },    EventCounter.SECRET_ROOMS_WALLS_OPENED },
+            { "arcades entered",                    _EventCounter.ARCADES_ENTERED },
+            { "shellgames played",                  _EventCounter.SHELLGAMES_PLAYED },
+            { { "blood donation", "machine used" }, _EventCounter.BLOOD_DONATION_MACHINE_USED },
+            { { "slot machines", "broken" },        _EventCounter.SLOT_MACHINES_BROKEN },
+            { { "golden chests", "opened" },        _EventCounter.CHESTS_OPENED_WITH_KEY },
+            { { "secret room", "walls broken" },    _EventCounter.SECRET_ROOMS_WALLS_OPENED },
         } },
         {
             "CHAOS", {
-            { "rocks destroyed",               EventCounter.ROCKS_DESTROYED },
-            { { "tinted rocks", "destroyed" }, EventCounter.TINTED_ROCKS_DESTROYED },
-            { { "super special", "rocks destroyed" }, EventCounter.SUPER_SPECIAL_ROCKS_DESTROYED },
-            { "poop destroyed",                EventCounter.POOP_DESTROYED },
-            { { "rainbow poop", "destroyed" }, EventCounter.RAINBOW_POOP_DESTROYED },
-            { "pills eaten",                   EventCounter.PILLS_EATEN },
-            { "cards used",                    EventCounter.CARDS_USED },
-            { "batteries collected",           EventCounter.BATTERIES_COLLECTED },
-            { "battery bums killed",           EventCounter.BATTERY_BUMS_KILLED },
+            { "rocks destroyed",               _EventCounter.ROCKS_DESTROYED },
+            { { "tinted rocks", "destroyed" }, _EventCounter.TINTED_ROCKS_DESTROYED },
+            { { "super special", "rocks destroyed" }, _EventCounter.SUPER_SPECIAL_ROCKS_DESTROYED },
+            { "poop destroyed",                _EventCounter.POOP_DESTROYED },
+            { { "rainbow poop", "destroyed" }, _EventCounter.RAINBOW_POOP_DESTROYED },
+            { "pills eaten",                   _EventCounter.PILLS_EATEN },
+            { "cards used",                    _EventCounter.CARDS_USED },
+            { "batteries collected",           _EventCounter.BATTERIES_COLLECTED },
+            { "battery bums killed",           _EventCounter.BATTERY_BUMS_KILLED },
         } },
         {
             "MISC", {
-            { "beds used",                              EventCounter.BEDS_USED },
-            { { "battery bum", "collectible payouts" }, EventCounter.BATTERY_BUM_COLLECTIBLE_PAYOUTS },
+            { "beds used",                              _EventCounter.BEDS_USED },
+            { { "battery bum", "collectible payouts" }, _EventCounter.BATTERY_BUM_COLLECTIBLE_PAYOUTS },
         } },
     }
 }
@@ -142,11 +151,11 @@ local function LoadAssets()
         font:Load("font/teammeatfont10.fnt")
     end
     if font:IsLoaded() and #(StatsMenuRep.StatSheetSprite:GetDefaultAnimation()) > 0 then
-        Isaac.RemoveCallback(REPENTOGON, ModCallbacks.MC_MAIN_MENU_RENDER, LoadAssets)
+        Isaac.RemoveCallback(REPENTOGON, _ModCallbacks.MC_MAIN_MENU_RENDER, LoadAssets)
     end
 end
 
-Isaac.AddCallback(REPENTOGON, ModCallbacks.MC_MAIN_MENU_RENDER, LoadAssets)
+Isaac.AddCallback(REPENTOGON, _ModCallbacks.MC_MAIN_MENU_RENDER, LoadAssets)
 
 
 local function RenderExtraStats()
@@ -154,8 +163,8 @@ local function RenderExtraStats()
         StatsMenu:GetStatsMenuSprite():GetLayer("Paper1"):SetVisible(true)
         return
     end
-    if MenuManager:GetActiveMenu() == MainMenuType.SAVES then StatsMenuRep.ScheduleRefresh = true end
-    if MenuManager:GetActiveMenu() == MainMenuType.STATS then
+    if MenuManager:GetActiveMenu() == _MainMenuType.SAVES then StatsMenuRep.ScheduleRefresh = true end
+    if MenuManager:GetActiveMenu() == _MainMenuType.STATS then
         if StatsMenuRep.ScheduleRefresh then
             local gameData = Isaac.GetPersistentGameData()
             MaxCategoryID = #StatsMenuRep.Stats + 1
@@ -172,14 +181,14 @@ local function RenderExtraStats()
             StatsMenuRep.ScheduleRefresh = false
         end
 
-        if IsActionTriggeredAll(ButtonAction.ACTION_MENULEFT) then
-            SFXManager():Play(SoundEffect.SOUND_PAPER_IN)
+        if IsActionTriggeredAll(_ButtonAction.ACTION_MENULEFT) then
+            SFXManager():Play(_SoundEffect.SOUND_PAPER_IN)
             StatsMenuRep.CurrentlyDisplayedCategoryID = (StatsMenuRep.CurrentlyDisplayedCategoryID - 1) % MaxCategoryID
             if StatsMenuRep.CurrentlyDisplayedCategoryID == 0 then
                 StatsMenuRep.CurrentlyDisplayedCategoryID = MaxCategoryID - 1
             end
-        elseif IsActionTriggeredAll(ButtonAction.ACTION_MENURIGHT) then
-            SFXManager():Play(SoundEffect.SOUND_PAPER_IN)
+        elseif IsActionTriggeredAll(_ButtonAction.ACTION_MENURIGHT) then
+            SFXManager():Play(_SoundEffect.SOUND_PAPER_IN)
             StatsMenuRep.CurrentlyDisplayedCategoryID = math.max(
                 (StatsMenuRep.CurrentlyDisplayedCategoryID + 1) % MaxCategoryID, 1)
         end
@@ -187,17 +196,17 @@ local function RenderExtraStats()
     StatsMenu:GetStatsMenuSprite():GetLayer("Paper1"):SetVisible(false)
 
     -- render stat page over original stat page, to allow for custom content
-    local posSheet = Isaac.WorldToMenuPosition(MainMenuType.STATS, StatsMenuRep.StatSheetSpritePos)
+    local posSheet = Isaac.WorldToMenuPosition(_MainMenuType.STATS, StatsMenuRep.StatSheetSpritePos)
     StatsMenuRep.StatSheetSprite:RenderLayer(1, posSheet)
-    local posLeftArrow = Isaac.WorldToMenuPosition(MainMenuType.STATS, StatsMenuRep.LeftArrowSpritePos)
+    local posLeftArrow = Isaac.WorldToMenuPosition(_MainMenuType.STATS, StatsMenuRep.LeftArrowSpritePos)
     StatsMenuRep.ArrowSprite:RenderLayer(6, posLeftArrow)
-    local posRightArrow = Isaac.WorldToMenuPosition(MainMenuType.STATS, StatsMenuRep.RightArrowSpritePos)
+    local posRightArrow = Isaac.WorldToMenuPosition(_MainMenuType.STATS, StatsMenuRep.RightArrowSpritePos)
     StatsMenuRep.ArrowSprite:RenderLayer(7, posRightArrow)
 
     local nextLineOffsetX = 0
     local nextLineOffsetY = 0
 
-    local pos = Isaac.WorldToMenuPosition(MainMenuType.STATS, StatsMenuPos)
+    local pos = Isaac.WorldToMenuPosition(_MainMenuType.STATS, StatsMenuPos)
 
     local categoryData = StatsMenuRep.Stats[StatsMenuRep.CurrentlyDisplayedCategoryID]
     font:DrawStringUTF8(categoryData[1], pos.X, pos.Y + nextLineOffsetY, fontcolor, 0, false)
@@ -234,5 +243,5 @@ end
 
 REPENTOGON.Extras.StatsMenu = StatsMenuRep
 
-Isaac.AddCallback(REPENTOGON, ModCallbacks.MC_PRE_GAME_EXIT, RefreshExtraStats)
-Isaac.AddCallback(REPENTOGON, ModCallbacks.MC_MAIN_MENU_RENDER, RenderExtraStats)
+Isaac.AddCallback(REPENTOGON, _ModCallbacks.MC_PRE_GAME_EXIT, RefreshExtraStats)
+Isaac.AddCallback(REPENTOGON, _ModCallbacks.MC_MAIN_MENU_RENDER, RenderExtraStats)
