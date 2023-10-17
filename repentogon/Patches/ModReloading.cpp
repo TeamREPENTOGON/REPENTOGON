4d2e5ba1be34b579acce1e320b812b54cab2a3e1
namishere <50415197+namishere@users.noreply.github.com>
2023-10-17 02:10:34 -0700

    a little better fix

--- repentogon/Patches/ModReloading.cpp
+++ repentogon/Patches/ModReloading.cpp
@@ -3,22 +3,21 @@
 #include "ModReloading.h"
 #include "../REPENTOGONOptions.h"
 
-bool didModUpdates = false;
-
 // We always want to restart after the game fully loads, this ensures that
 HOOK_METHOD(ModManager, UpdateWorkshopMods, () -> void) {
 	super();
-	didModUpdates = true;
+	repentogonOptions.Write("internal", "didmodreset", "0");
+	// UpdateWorkshopMods gets called unconditionally by IsaacStartup, so we can guarantee that the bool is properly reset by doing it here
 }
 
 HOOK_METHOD(ModManager, Reset, () -> void) {
 	bool res = repentogonOptions.GetBool("internal", "didmodreset");
-	if (!res || didModUpdates) {
+	if (!res) {
 		repentogonOptions.Write("internal", "didmodreset", "1");
 		GameRestart(); //if we ever walk back from this, for whateevr reason, we will need to recheck some stuff regarding a few xml reloads on xmldata
 	}
 	else {
 		super();
+		// Reset gets conditionally called in UpdateWorkshopMods, so setting didmodreset here will have inconsistent results
 	}
-	repentogonOptions.Write("internal", "didmodreset", "0");
 }
