81f5e320bac363a447fed0cb375d9ca7bbeebb6c
Sylmir <sylmir.dev@gmail.com>
2023-12-17 11:17:21 +0100

    Redirect Lua5.3 to Lua 5.4

--- launcher/dllmain.cpp
+++ launcher/dllmain.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <ImageHlp.h>
 #include "ConsoleWindow.h"
 #include "Updater.h"
 #include "utils.h"
@@ -29,10 +30,121 @@ static bool HasCommandLineArgument(const char *arg)
 
 static std::vector<std::pair<std::string, HMODULE>> mods;
 
+DWORD RedirectLua(HMODULE* outLua) {
+	HMODULE isaac = GetModuleHandle(NULL);
+	if (!isaac) {
+		fprintf(stderr, "RedirectLua: unable to get main module handle %d\n", GetLastError());
+		return -1;
+	}
+
+	PIMAGE_NT_HEADERS headers = ImageNtHeader(isaac);
+	if (!headers) {
+		fprintf(stderr, "RedirectLua: unable to get NT headers %d\n", GetLastError());
+		return -1;
+	}
+
+	HMODULE lua = LoadLibrary("Lua5.4.dll");
+	if (!lua) {
+		fprintf(stderr, "RedirectLua: unable to load Lua 5.4 DLL %d\n", GetLastError());
+		return -1;
+	}
+
+	*outLua = lua;
+
+	PIMAGE_DATA_DIRECTORY importDataDirectory = headers->OptionalHeader.DataDirectory + IMAGE_DIRECTORY_ENTRY_IMPORT;
+	PIMAGE_IMPORT_DESCRIPTOR descriptor = (PIMAGE_IMPORT_DESCRIPTOR)((char*)isaac + importDataDirectory->VirtualAddress);
+
+	while (descriptor->Characteristics) {
+		const char* name = (const char*)isaac + descriptor->Name;
+		if (strcmp(name, "Lua5.3.3r.dll") == 0) {
+			break;
+		}
+
+		++descriptor;
+	}
+
+	if (descriptor->Characteristics == 0) {
+		fprintf(stderr, "RedirectLua: unable to find import table for Lua5.3.3r.dll");
+		return -1;
+	}
+
+	PIMAGE_THUNK_DATA originalData = (PIMAGE_THUNK_DATA)((char*)isaac + descriptor->OriginalFirstThunk);
+	PIMAGE_THUNK_DATA resolvedData = (PIMAGE_THUNK_DATA)((char*)isaac + descriptor->FirstThunk);
+
+	struct FunctionMapping {
+		PIMAGE_THUNK_DATA thunk;
+		void* addr;
+	};
+
+	std::vector<FunctionMapping> functionMapping;
+
+	while (originalData->u1.Ordinal != 0) {
+		DWORD data = originalData->u1.Ordinal;
+		if (data & 0x80000000) {
+			fprintf(stderr, "RedirectLua: found import by ordinal, unable to proceed\n");
+			return -1;
+		}
+
+		PIMAGE_IMPORT_BY_NAME import = (PIMAGE_IMPORT_BY_NAME)((char*)isaac + originalData->u1.AddressOfData);
+		const char* symbolName = import->Name;
+
+		void* newFunction = GetProcAddress(lua, symbolName);
+		if (!newFunction) {
+			fprintf(stderr, "RedirectLua: unable to find symbol %s in Lua 5.4\n", symbolName);
+			return -1;
+		}
+
+		FunctionMapping mapping;
+		mapping.thunk = resolvedData;
+		mapping.addr = newFunction;
+		functionMapping.push_back(mapping);
+
+		++originalData;
+		++resolvedData;
+	}
+
+	for (FunctionMapping& mapping : functionMapping) {
+		MEMORY_BASIC_INFORMATION info;
+		DWORD queryResult = VirtualQuery(mapping.thunk, &info, sizeof(info));
+		if (queryResult == 0) {
+			fprintf(stderr, "[fatal] RedirectLua: VirtualQuery failed %d\n", GetLastError());
+			abort();
+		}
+
+		DWORD originalProtect;
+		BOOL protectResult = VirtualProtect(info.BaseAddress, info.RegionSize, PAGE_READWRITE, &originalProtect);
+		if (!protectResult) {
+			fprintf(stderr, "[fatal] RedirectLua: VirtualProtect failed %d\n", GetLastError());
+			abort();
+		}
+
+		*(void**)mapping.thunk = mapping.addr;
+
+		protectResult = VirtualProtect(info.BaseAddress, info.RegionSize, originalProtect, &originalProtect);
+		if (!protectResult) {
+			fprintf(stderr, "[fatal] RedirectLua: VirtualProtect reverse failed %d\n", GetLastError());
+			abort();
+		}
+	}
+
+	return 0;
+}
+
+static HMODULE luaHandle = NULL;
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	if(ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
+		DWORD redirectResult = RedirectLua(&luaHandle);
+		if (!redirectResult) {
+			if (luaHandle) {
+				FreeLibrary(luaHandle);
+				luaHandle = NULL;
+			}
+
+			fprintf(stderr, "dsound: error while redirecting Lua calls, falling back to original Lua 5.3.3 DLL\n");
+		}
 		// Prevents the module from being unloaded by the game
 		HMODULE hModule_Dupe;
 		GetModuleHandleEx(NULL, "dsound.dll", &hModule_Dupe);
@@ -100,14 +212,17 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 
 		printf("Loaded all mods in %d msecs\n", GetTickCount() - startTime);
 	}
-	/*else if(ul_reason_for_call == DLL_PROCESS_DETACH)
+	else if(ul_reason_for_call == DLL_PROCESS_DETACH)
 	{
-		printf("unloading libs\n");
+		/* printf("unloading libs\n");
 		for(auto it = mods.begin() ; it != mods.end() ; ++it)
 		{
 			FreeLibrary(*it);
+		} */
+		if (luaHandle) {
+			FreeLibrary(luaHandle);
 		}
-	}*/
+	}
 
 	return TRUE;
 }
