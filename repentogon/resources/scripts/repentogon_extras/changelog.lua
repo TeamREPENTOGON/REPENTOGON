c991f8d20c9dc1602991ae8b979f1fc1a0c379e2
jerbmega <jsilliman@protonmail.com>
2023-12-19 22:44:30 -0500

    fix commit not updating in changelog.lua
    :crucified_ministro:

--- repentogon/resources/scripts/repentogon_extras/changelog.lua
+++ repentogon/resources/scripts/repentogon_extras/changelog.lua
@@ -131,7 +131,7 @@ function ChangeLog.MenuRender()
     end
     Cl.NoteSprite:Render(Isaac.WorldToMenuPosition(_MainMenuType.TITLE, Cl.NoteOffset))
     local versionPosition = Isaac.WorldToMenuPosition(_MainMenuType.TITLE, Cl.VersionOffset)
-    Cl.VersionFont:DrawStringUTF8("REPENTOGON 2d210f8", versionPosition.X + 1, versionPosition.Y + 1,
+    Cl.VersionFont:DrawStringUTF8("REPENTOGON dev build", versionPosition.X + 1, versionPosition.Y + 1,
         KColor(0, 0, 0, 0.3), 200, true)
     Cl.VersionFont:DrawStringUTF8("REPENTOGON 2d210f8", versionPosition.X, versionPosition.Y,
         KColor(67 / 255, 5 / 255, 5 / 255, 1), 200, true)
