cbeacd2638865a44b821223c9a1efe67b390f0db
fly_6 <36567300+epfly6@users.noreply.github.com>
2024-01-25 16:06:36 +0300

    DIsabling pickupischest feature

--- repentogon/Patches/PickupTags.cpp
+++ repentogon/Patches/PickupTags.cpp
@@ -4,9 +4,10 @@
 
 constexpr char PICKUP_IS_CHEST[] = "pickupischest";
 
-HOOK_STATIC(Entity_Pickup, IsChest, (unsigned int variant) -> bool) {
+/*HOOK_STATIC(Entity_Pickup, IsChest, (unsigned int variant) -> bool) {
 	if (XMLStuff.EntityData->HasCustomTag(5, variant, 1, PICKUP_IS_CHEST)) {
 		return true;
 	}
 	return super(variant);
 }
+*/
