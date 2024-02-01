70f9a9c4e4ac5440827698fd6a6459349897bea0
namishere <50415197+namishere@users.noreply.github.com>
2024-01-31 22:48:31 -0800

    EntityEffect sigs & c++ funcs

--- repentogon/LuaInterfaces/Entities/LuaEntityBomb.cpp
+++ repentogon/LuaInterfaces/Entities/LuaEntityBomb.cpp
@@ -1,30 +1,30 @@
 #include "IsaacRepentance.h"
 #include "LuaCore.h"
 
-const BitSet128 zero(0, 0, 0, 0);
+BitSet128 zero(0, 0, 0, 0);
 
 extern "C" {
 	BitSet128* L_EntityBomb_GetTearFlags(Entity_Bomb* bomb) {
 		return &bomb->_tearFlags;
 	}
 
-	void L_EntityBomb_SetTearFlags(Entity_Bomb* bomb, BitSet128 flags) {
-		bomb->_tearFlags = flags;
+	void L_EntityBomb_SetTearFlags(Entity_Bomb* bomb, BitSet128* flags) {
+		bomb->_tearFlags = *flags;
 	}
 
-	void L_EntityBomb_AddTearFlags(Entity_Bomb* bomb, BitSet128 flags) {
-		bomb->_tearFlags &= flags;
+	void L_EntityBomb_AddTearFlags(Entity_Bomb* bomb, BitSet128* flags) {
+		bomb->_tearFlags &= *flags;
 	}
 
-	void L_EntityBomb_ClearTearFlags(Entity_Bomb* bomb, BitSet128 flags) {
-		bomb->_tearFlags &= ~flags;
+	void L_EntityBomb_ClearTearFlags(Entity_Bomb* bomb, BitSet128* flags) {
+		bomb->_tearFlags &= ~(*flags);
 	}
 
-	bool L_EntityBomb_HasTearFlags(Entity_Bomb* bomb, BitSet128 flags) {
-		return (bomb->_tearFlags & flags) != zero;
+	bool L_EntityBomb_HasTearFlags(Entity_Bomb* bomb, BitSet128* flags) {
+		return (bomb->_tearFlags & *flags) != zero;
 	}
 
-	int L_EntityBomb_SetExplosionCountdown(Entity_Bomb* bomb, int value) {
+	void L_EntityBomb_SetExplosionCountdown(Entity_Bomb* bomb, int value) {
 		bomb->_explosionCountdown1 = value;
 		bomb->_explosionCountdown2 = value;
 	}
