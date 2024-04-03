73616c9df530afb52064f100f3dbf760724659da
fly_6 <36567300+epfly6@users.noreply.github.com>
2024-04-03 17:35:57 +0300

    EntityTear extra sprites
    Added:
     -GetTearHaloSprite
     -GetTearEffectSprite
     -GetDeadEyeSprite

--- repentogon/LuaInterfaces/Entities/LuaEntityTear.cpp
+++ repentogon/LuaInterfaces/Entities/LuaEntityTear.cpp
@@ -17,6 +17,24 @@ LUA_FUNCTION(Lua_TearMakeMultidimensionalCopy)
 	return 1;
 }
 
+LUA_FUNCTION(Lua_GetTearHaloSprite) {
+	Entity_Tear* tear = lua::GetUserdata<Entity_Tear*>(L, 1, lua::Metatables::ENTITY_TEAR, "EntityTear");
+	lua::luabridge::UserdataPtr::push(L, &tear->_tearHaloANM2, lua::Metatables::SPRITE);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_GetTearEffectSprite) {
+	Entity_Tear* tear = lua::GetUserdata<Entity_Tear*>(L, 1, lua::Metatables::ENTITY_TEAR, "EntityTear");
+	lua::luabridge::UserdataPtr::push(L, &tear->_tearEffectANM2, lua::Metatables::SPRITE);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_GetDeadEyeSprite) {
+	Entity_Tear* tear = lua::GetUserdata<Entity_Tear*>(L, 1, lua::Metatables::ENTITY_TEAR, "EntityTear");
+	lua::luabridge::UserdataPtr::push(L, &tear->_deadEyeANM2, lua::Metatables::SPRITE);
+	return 1;
+}
+
 HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
 	super();
 
@@ -25,6 +43,9 @@ HOOK_METHOD(LuaEngine, RegisterClasses, () -> void) {
 	luaL_Reg functions[] = {
 		{ "GetDeadEyeIntensity", Lua_TearGetDeadEyeIntensity },
 		{ "MakeMultidimensionalCopy", Lua_TearMakeMultidimensionalCopy },
+		{ "GetTearHaloSprite", Lua_GetTearHaloSprite },
+		{ "GetTearEffectSprite", Lua_GetTearEffectSprite },
+		{ "GetDeadEyeSprite", Lua_GetDeadEyeSprite },
 		{ NULL, NULL }
 	};
 	lua::RegisterFunctions(_state, lua::Metatables::ENTITY_TEAR, functions);
