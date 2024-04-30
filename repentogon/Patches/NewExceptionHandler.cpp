98db9da8c74003ed281eb8d30c0cf997db4c4cfd
im-tem <anton255260@gmail.com>
2024-04-30 22:00:13 +0300

    more work on new handler, disable it for now,

--- repentogon/Patches/NewExceptionHandler.cpp
+++ repentogon/Patches/NewExceptionHandler.cpp
@@ -1,12 +1,18 @@
 #include "IsaacRepentance.h"
 #include "HookSystem.h"
 #include "LuaCore.h"
+#include "processthreadsapi.h"
+#include "../Version.h"
+#include "../MiscFunctions.h"
+#include "minidumpapiset.h"
 
 bool exception_message_shown = false;
-HOOK_GLOBAL_PRIORITY(RepCaughtException, 9999, (int x)->void,__cdecl) {
+//HOOK_GLOBAL_PRIORITY(RepCaughtException, 9999, (int x)->void,__cdecl) {
+void ___todo_exception_handler(int x){	//placeholder
+
 	char message[1024]="Something went wrong in the vanilla part of the game's code! Lua traceback unavailable!";
 	char buffer[1024];
-	super(x);
+//	super(x);
 	if (!exception_message_shown) {
 		if (g_LuaEngine != nullptr) {
 			lua_Debug debuginfo;
@@ -22,7 +28,43 @@ HOOK_GLOBAL_PRIORITY(RepCaughtException, 9999, (int x)->void,__cdecl) {
 				is_valid_stack = lua_getstack(g_LuaEngine->_state, stacklevel, &debuginfo);
 			};
 		};
-		MessageBoxA(nullptr, message, "A super-basic crash handler!", MB_OK);
 		exception_message_shown = true;
+
+		typedef BOOL (*MiniDumpWriteDump)(HANDLE, DWORD, HANDLE, MINIDUMP_TYPE, PMINIDUMP_EXCEPTION_INFORMATION, PMINIDUMP_USER_STREAM_INFORMATION, PMINIDUMP_CALLBACK_INFORMATION);
+
+		HMODULE dbghelp = LoadLibraryA("dbghelp");
+
+		if (dbghelp == 0x0) {
+			return;
+		};
+
+		MiniDumpWriteDump gen_minidump = (MiniDumpWriteDump)GetProcAddress(dbghelp, "MiniDumpWriteDump");
+		if (gen_minidump == 0x0) {
+			return;
+		};
+		SYSTEMTIME time_holder;
+		GetLocalTime(&time_holder);
+
+		DWORD cur_pid = GetCurrentProcessId();
+		DWORD cur_thread = GetCurrentThreadId();
+
+		char dump_name_fmt[] = "rgon-v%s-%04d%02d%02d-%02d%02d%02d-%ld-%ld.dmp";
+		char dump_name[1024]="";
+		snprintf(dump_name,1023,dump_name_fmt,VERSION,time_holder.wYear,time_holder.wMonth,time_holder.wDay,time_holder.wHour,time_holder.wMinute,time_holder.wSecond,cur_pid,cur_thread);
+
+		std::string dump_path;
+		dump_path = REPENTOGON::GetRepentogonDataPath();
+		dump_path += "\\..\\";
+		dump_path += dump_name;
+		//todo: x is some value from which exception info should be retreivable, not trivially accessible though!
+		HANDLE file_handle=CreateFileA(dump_path.c_str(), 0x40000000, 1, (LPSECURITY_ATTRIBUTES)0x0, 0x2, 0x80, 0x0);
+		(MiniDumpWriteDump)gen_minidump(GetCurrentProcess(), cur_pid, file_handle, _MINIDUMP_TYPE::MiniDumpWithDataSegs, nullptr,nullptr, nullptr);
+		CloseHandle(file_handle);
+
+		MessageBoxA(nullptr, message, "A super-basic crash handler!", MB_OK);
+
+		DWORD exitcode;
+		ExitProcess(GetExitCodeProcess(GetCurrentProcess(),&exitcode));
+		return;
 	};
 };
