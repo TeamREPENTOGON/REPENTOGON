7be7411380c0ac928fdc746f12fe3a09a36a4ffc
Sylmir <sylmir.dev@gmail.com>
2024-05-12 08:09:47 +0200

    Define NOMINMAX so MSVC shuts up about min/max in rapidjson

--- repentogon/LuaInterfaces/Entities/LuaEntityPlayer.cpp
+++ repentogon/LuaInterfaces/Entities/LuaEntityPlayer.cpp
@@ -885,7 +885,7 @@ LUA_FUNCTION(Lua_PlayerTriggerRoomClear) {
 
 LUA_FUNCTION(Lua_PlayerShuffleCostumes) {
 	Entity_Player* plr = lua::GetUserdata<Entity_Player*>(L, 1, lua::Metatables::ENTITY_PLAYER, "EntityPlayer");
-	unsigned int seed = (unsigned int)luaL_optinteger(L, 2, min(Isaac::genrand_int32(), 1));
+	unsigned int seed = (unsigned int)luaL_optinteger(L, 2, std::min(Isaac::genrand_int32(), 1U));
 	plr->ShuffleCostumes(seed);
 
 	return 0;
