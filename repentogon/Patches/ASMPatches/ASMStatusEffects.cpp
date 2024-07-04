2cd01f5e4823f95abff7eeeb4fc6c70ecb0703f3
namishere <50415197+namishere@users.noreply.github.com>
2024-07-03 20:34:54 -0700

    logging improvements, AddBleeding

--- repentogon/Patches/ASMPatches/ASMStatusEffects.cpp
+++ repentogon/Patches/ASMPatches/ASMStatusEffects.cpp
@@ -7,6 +7,7 @@
 
 struct StatusEffectPatchInfo {
 	char* signature;
+	char* funcName;
 	int sigOffset;
 	ASMPatch::Registers entityReg;
 	ASMPatch::Registers targetReg;
@@ -14,10 +15,11 @@ struct StatusEffectPatchInfo {
 	unsigned int jumpOffset;
 };
 
-StatusEffectPatchInfo patches[1] = {
+StatusEffectPatchInfo patches[2] = {
 	{"8bce8bd0e8????????84c074??8b8e????????85c974??0f1f44??008bf18b8e????????85c975??85d20f84????????8b86????????83e00183c8000f85????????3986????????0f8f????????8b8e????????85d27e??8d04??b92c0100003bc10f4cc8eb??f7da3bca0f4cca898e????????8d4c24??0f2805????????6a010f114424??6a000f57c0",
-	0, ASMPatch::Registers::ECX, ASMPatch::Registers::ESI, ASMPatch::SavedRegisters::Registers::ESI, 0x28},
-
+	"AddBaited", 0, ASMPatch::Registers::ECX, ASMPatch::Registers::ESI, ASMPatch::SavedRegisters::Registers::ESI, 0x28},
+	{"8bce8bd0e8????????84c074??8b8e????????85c974??90",
+	"AddBleeding", 0, ASMPatch::Registers::ECX, ASMPatch::Registers::ESI, ASMPatch::SavedRegisters::Registers::ESI, 0x24},
 };
 
 /* /////////////////////
@@ -48,14 +50,13 @@ void ASMPatchInlinedGetStatusEffectTarget(void* addr, ASMPatch::Registers entity
 
 void PatchInlinedGetStatusEffectTarget()
 {
-	unsigned char itr = 1;
+	ZHL::Logger logger;
 	for (StatusEffectPatchInfo i : patches) {
 		SigScan scanner(i.signature);
 		scanner.Scan();
 		void* addr = (char*)scanner.GetAddress() + i.sigOffset;
 
-		ZHL::Logger logger;
-		logger.Log("Patching inlined GetStatusEffectTarget #%d at %p\n", itr, addr, i.jumpOffset);
+		logger.Log("Patching inlined GetStatusEffectTarget in %s at %p\n", i.funcName, addr);
 		ASMPatchInlinedGetStatusEffectTarget(addr, i.entityReg, i.targetReg, i.saveReg, i.jumpOffset);
 	};
 }
