048df1bdadfe76a44e2a373ea8f569641e1b3872
fly_6 <36567300+epfly6@users.noreply.github.com>
2024-07-05 19:20:10 +0300

    submit updated entityList zhl

--- repentogon/Patches/ExtraDebugRender.cpp
+++ repentogon/Patches/ExtraDebugRender.cpp
@@ -2,9 +2,9 @@
 #include "HookSystem.h"
 
 #include "../REPENTOGONOptions.h"
-/*
-HOOK_METHOD(EntityList, QueryRadius, (EntityList_EL* result, Vector* Position, float Radius, unsigned int Partitions) -> void) {
-	super(result, Position, Radius, Partitions);
+
+HOOK_METHOD(EntityList, QueryRadius, (Vector* Position, float Radius, unsigned int Partitions) -> EntityList_EL) {
+	printf("FindInRadius: %.2f %.2f\n", Position->x, Position->y); // DEBUG
 	if (repentogonOptions.renderDebugFindInRadius && (*g_Game->GetDebugFlags() >> 5 & 1)) {
 		//Vector vec{ 1,1 };
 		//Capsule capsule{ Position, &vec, 0, Radius };
@@ -12,6 +12,5 @@ HOOK_METHOD(EntityList, QueryRadius, (EntityList_EL* result, Vector* Position, f
 		*shape->GetTimeout() = 1;
 		shape->Circle(Position, Radius);
 	}
-
+	return super(Position, Radius, Partitions);
 }
-*/ // causes Evil Eye Funkyness
