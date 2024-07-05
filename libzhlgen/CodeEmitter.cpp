1b2da557b0be70310c62225ee7a53cd2746f5da3
Sylmir <sylmir.dev@gmail.com>
2024-07-05 19:37:04 +0200

    [Parser] Fix placement of implicit output ArgData
    For non x86-64 w/ output functions, the implicit output arg data of
    the function was emitted as the last arg data, causing it to be
    incorrectly pushed on the stack first instead of last during the
    generation of hooks / supers.
    
    This patch fixes this, so now implicit output arg data is the first
    in the array of arg data.
    
    Additionally, the commit fixes the placement of commas in the array,
    as they used to appear at the very end of each line, rather than
    appearing after the definition of the ArgData (which could result
    in invalid code under certain circumstances).

--- libzhlgen/CodeEmitter.cpp
+++ libzhlgen/CodeEmitter.cpp
@@ -1291,7 +1291,13 @@ std::tuple<bool, uint32_t, uint32_t> CodeEmitter::EmitArgData(Function const& fn
                             next = EAX;
                         }
 
-                        Emit(", 0 } /* " + param._name + " */");
+                        Emit(", 0 }");
+                        
+                        if (i != fn._params.size() - 1) {
+                            Emit(",");
+                        }
+
+                        Emit("/* " + param._name + " */");
                         /* Do not actually increase the size of the stack here :
                          * because the trampoline is generated as a fastcall
                          * function, parameters are already placed in registers
@@ -1301,12 +1307,12 @@ std::tuple<bool, uint32_t, uint32_t> CodeEmitter::EmitArgData(Function const& fn
                          // stackSize += 4;
                     }
                     else {
-                        EmitParamData(fn, param, &fnStackSize, &stackSize);
+                        EmitParamData(fn, param, &fnStackSize, &stackSize, fn._params.size() - 1);
                     }
 
-                    if (hasImplicitOutput || i != fn._params.size() - 1) {
+                    /* if (hasImplicitOutput || i != fn._params.size() - 1) {
                         Emit(", ");
-                    }
+                    } */
                     EmitNL();
                 }
             }
@@ -1393,7 +1399,7 @@ std::tuple<bool, uint32_t, uint32_t> CodeEmitter::EmitArgData(Function const& fn
                         ++position;
                     }
                     else {
-                        EmitParamData(fn, param, &fnStackSize, &stackSize);
+                        EmitParamData(fn, param, &fnStackSize, &stackSize, i != fn._params.size() - 1);
                     }
 
                     if (i != fn._params.size() - 1) {
@@ -1408,6 +1414,19 @@ std::tuple<bool, uint32_t, uint32_t> CodeEmitter::EmitArgData(Function const& fn
             }
         }
         else {
+            if (hasImplicitOutput && (!fn._convention || fn._convention != X86_64_OUTPUT)) {
+                EmitTab();
+                Emit("{ HookSystem::NoRegister(), 1 }"); // 1: pointer
+                if (fn._params.size() != 0) {
+                    Emit(",");
+                }
+                Emit(" /* implicit_output */");
+                EmitNL();
+
+                stackSize += 4; // Pointer
+                fnStackSize += 4;
+            }
+
             for (size_t i = 0; i < fn._params.size(); ++i) {
                 EmitTab();
                 FunctionParam const& param = fn._params[i];
@@ -1424,28 +1443,22 @@ std::tuple<bool, uint32_t, uint32_t> CodeEmitter::EmitArgData(Function const& fn
                     std::string regStr = RegisterToString(reg);
                     std::transform(regStr.begin(), regStr.end(), regStr.begin(), ::toupper);
                     Emit(regStr);
-                    Emit(", 0 } /* " + param._name + " */");
+                    Emit(", 0 }");
+                    
+                    if (i != fn._params.size() - 1) {
+                        Emit(", ");
+                    }
+
+                    Emit("/* " + param._name + " */");
 
                     stackSize += 4;
                 }
                 else {
-                    EmitParamData(fn, param, &fnStackSize, &stackSize);
+                    EmitParamData(fn, param, &fnStackSize, &stackSize, i != fn._params.size() - 1);
                 }
 
-                if (hasImplicitOutput || i != fn._params.size() - 1) {
-                    Emit(", ");
-                }
                 EmitNL();
             }
-        }
-
-        if (hasImplicitOutput && (!fn._convention || fn._convention != X86_64_OUTPUT)) {
-            EmitTab();
-            Emit("{ HookSystem::NoRegister(), 1 } // implicit_output"); // 1: pointer
-            EmitNL();
-
-            stackSize += 4; // Pointer
-            fnStackSize += 4;
         }
 
         DecrDepth();
@@ -1510,14 +1523,19 @@ Function const* CodeEmitter::GetFunction(std::variant<Signature, Skip, Function>
     }
 }
 
-void CodeEmitter::EmitParamData(Function const& fn, FunctionParam const& param, uint32_t* fnStackSize, uint32_t* stackSize) {
+void CodeEmitter::EmitParamData(Function const& fn, FunctionParam const& param, uint32_t* fnStackSize, uint32_t* stackSize, bool comma) {
     int paramSize = (int)std::ceil(param._type->size() / 4.f);
     if (paramSize == 0) {
         std::cerr << "Got a size of 0 for param " << param._name << " of type " << param._type->ToString(false) << " of function " << fn._name << ", true size is " << param._type->size() << std::endl;
     }
     Emit("{ HookSystem::NoRegister(), ");
     Emit(std::to_string(paramSize));
-    Emit(" } /* " + param._name + " */");
+    Emit(" }");
+    
+    if (comma) {
+        Emit(",");
+    }
+    Emit("/* " + param._name + " */");
 
     *stackSize += paramSize * 4;
     *fnStackSize += paramSize * 4;
