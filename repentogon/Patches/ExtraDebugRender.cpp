e595f9e9951cdeee7d111ad4902c17768318f849
fly_6 <36567300+epfly6@users.noreply.github.com>
2024-07-05 21:39:03 +0300

    Re-enabled "Extra debug render for FindInRadius"

--- repentogon/Patches/ExtraDebugRender.cpp
+++ repentogon/Patches/ExtraDebugRender.cpp
@@ -4,7 +4,6 @@
 #include "../REPENTOGONOptions.h"
 
 HOOK_METHOD(EntityList, QueryRadius, (Vector* Position, float Radius, unsigned int Partitions) -> EntityList_EL) {
-	printf("FindInRadius: %.2f %.2f\n", Position->x, Position->y); // DEBUG
 	if (repentogonOptions.renderDebugFindInRadius && (*g_Game->GetDebugFlags() >> 5 & 1)) {
 		//Vector vec{ 1,1 };
 		//Capsule capsule{ Position, &vec, 0, Radius };
