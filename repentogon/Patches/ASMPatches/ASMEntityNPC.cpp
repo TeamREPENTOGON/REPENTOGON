65338cdf7e86a686194e0fb9406764e398263afc
namishere <50415197+namishere@users.noreply.github.com>
2024-07-21 16:39:22 -0700

    A Promise Fulfilled
    Fix Hush lasers not respecting Room slowdown

--- repentogon/Patches/ASMPatches/ASMEntityNPC.cpp
+++ repentogon/Patches/ASMPatches/ASMEntityNPC.cpp
@@ -18,7 +18,7 @@ void __stdcall SetHushPanicLevel() {
 	hushPanicLevel = repentogonOptions.hushPanicStateFix ? 0.005f : 0.5f;
 }
 
-void PerformHushPatch(void* addr) {
+void PerformHushPanicPatch(void* addr) {
 
 	void* panicPtr = &hushPanicLevel;
 	ASMPatch::SavedRegisters reg(ASMPatch::SavedRegisters::GP_REGISTERS_STACKLESS, true);
@@ -31,6 +31,28 @@ void PerformHushPatch(void* addr) {
 	sASMPatcher.PatchAt(addr, &patch);
 }
 
+int __stdcall IsRoomSlow() {
+	Room* room = g_Game->_room;
+	return (room->_slowdownDuration > 0 || room->GetBrokenWatchState() == 1);
+}
+
+const float hushLaserAdjust = 0.513f * 0.75; // a base value i got empirically a while back + a slight extra bit of wiggle room
+void PatchHushLaserSpeed() {
+	const char* signature = "e8????????f30f1185????????f30f1005????????f30f1185????????8b95";
+	SigScan scanner(signature);
+	scanner.Scan();
+	void* addr = scanner.GetAddress();
+	const float* floatPtr = &hushLaserAdjust;
+	ASMPatch patch;
+	patch.AddBytes("\xF3\x0F\x10\x81\xAC\x14").AddZeroes(2); // movss xmm0,dword ptr ds:[ecx+14AC]
+	patch.AddInternalCall(IsRoomSlow); // call SetHushPanicLevel()
+	patch.AddBytes("\x84\xC0"); // test al, al
+	patch.AddBytes("\x74\x08"); // je, eip+0x8
+	patch.AddBytes("\xF3\x0F\x59\x05").AddBytes(ByteBuffer().AddAny((char*)&floatPtr, 4)); // mulss xmm0, dword ptr [0xXXXXXXXX]
+	patch.AddRelativeJump((char*)addr + 0x5); // jmp isaac-ng.XXXXXXXX
+	sASMPatcher.PatchAt(addr, &patch);
+}
+
 void ASMPatchHushBug() {
 	SigScan scanner1("f30f1005????????0f2f85????????0f86????????33c9");
 	SigScan scanner2("f30f1005????????0f2f85????????0f86????????837f??00");
@@ -38,8 +60,9 @@ void ASMPatchHushBug() {
 	scanner2.Scan();
 	void* addrs[2] = { scanner1.GetAddress(), scanner2.GetAddress() };
 	printf("[REPENTOGON] Patching the Hush panic state bug at %p, %p\n", addrs[0], addrs[1]);
-	PerformHushPatch(addrs[0]);
-	PerformHushPatch(addrs[1]);
+	PerformHushPanicPatch(addrs[0]);
+	PerformHushPanicPatch(addrs[1]);
+	PatchHushLaserSpeed();
 }
 
 void __stdcall FireProjectilesEx_Internal(std::vector<Entity_Projectile*> const& projectiles) {
