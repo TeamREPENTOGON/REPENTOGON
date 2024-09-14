cdcda093641cc873a740b447e3e6c16978c114cb
Sylmir <sylmir.dev@gmail.com>
2024-09-14 21:12:08 +0200

    Let go of the past. Kill it, if you have to.
    Summary: This removes the previous dsound based approach
    of loading ZHL from the game, and replaces it with the zhlLoader
    DLL that is invoked by Isaac itself.
    
    It also removes the updater, as that part is no handled by
    the launcher itself.

--- dsound/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define ISAAC_UNKNOWN 0
-#define ISAAC_REBIRTH 1
-#define ISAAC_AFTERBIRTH 2
-//#define ISAAC_AFTERBIRTHP 3 fuck you
-#define ISAAC_REPENTANCE 4
-
-int GetIsaacVersion();
