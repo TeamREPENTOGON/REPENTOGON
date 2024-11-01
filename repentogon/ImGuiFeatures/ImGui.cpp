5fc91074ff6dd00603ff7874b0a0ac7ef1d22996
im-tem <anton255260@gmail.com>
2024-11-01 12:11:54 +0300

    Revert "imgui: pick implementation on the fly"
    This reverts commit 0120c29cce0c5b9b2b4f9469ed7ef5f64475a308.

--- repentogon/ImGuiFeatures/ImGui.cpp
+++ repentogon/ImGuiFeatures/ImGui.cpp
@@ -23,7 +23,6 @@
 #include "imgui.h"
 #include "imgui_freetype.h"
 #include "imgui_impl_opengl2.h"
-#include "imgui_impl_opengl3.h"
 #include "imgui_impl_win32.h"
 #include "../MiscFunctions.h"
 #include "../REPENTOGONOptions.h"
@@ -31,8 +30,6 @@
 // this blogpost https://werwolv.net/blog/dll_injection was a big help, thanks werwolv!
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-
 
 HWND window;
 WNDPROC windowProc;
@@ -480,20 +477,6 @@ void RenderLuamodErrorPopup() {
 ImFont* imFontUnifont = NULL;
 HGLRC imguiRenderContext=NULL;
 
-typedef bool (*ImGui_impl_Init)(const char* glsl_version);	//function prototypes for pointers
-typedef void (*ImGui_impl_NewFrame)();
-typedef void (*ImGui_impl_RenderDrawData)(ImDrawData *imdrawdata);
-
-bool imgui_gl2_init_wrapper(const char* glsl_version) {
-	return ImGui_ImplOpenGL2_Init();
-};
-
-ImGui_impl_Init imgui_init = &imgui_gl2_init_wrapper;
-ImGui_impl_NewFrame imgui_newframe = &ImGui_ImplOpenGL2_NewFrame;
-ImGui_impl_RenderDrawData imgui_renderdrawdata = &ImGui_ImplOpenGL2_RenderDrawData;
-
-
-
 void __stdcall RunImGui(HDC hdc) {
 	HGLRC oldcontext;
 	static std::map<int, ImFont*> fonts;
@@ -506,19 +489,10 @@ void __stdcall RunImGui(HDC hdc) {
 		HWND window = WindowFromDC(hdc);
 		windowProc = (WNDPROC)SetWindowLongPtr(window,
 			GWLP_WNDPROC, (LONG_PTR)windowProc_hook);
-		char* glVer = (char*)glGetString(GL_VERSION); // ver = "3.2.0"
-
-		int glMajorVer = glVer[0] - '0';
-		printf("Reported OpenGL: %s\nParsed major version is %d\n", glVer,glMajorVer);
-		if (glMajorVer > 2) {	//if opengl > 2 then it can use opengl 3.0 impl of imgui
-			imgui_init = &ImGui_ImplOpenGL3_Init;
-			imgui_newframe = &ImGui_ImplOpenGL3_NewFrame;
-			imgui_renderdrawdata = &ImGui_ImplOpenGL3_RenderDrawData;
-		};
-//		imguiRenderContext = wglCreateContext(hdc);
+		imguiRenderContext = wglCreateContext(hdc);
 		ImGui::CreateContext();
 		ImGui_ImplWin32_Init(window);
-		imgui_init(nullptr);
+		ImGui_ImplOpenGL2_Init();
 		ImGui::StyleColorsDark();
 		ImGui::GetStyle().AntiAliasedFill = false;
 		ImGui::GetStyle().AntiAliasedLines = false;
@@ -589,8 +563,9 @@ void __stdcall RunImGui(HDC hdc) {
 	}
 
 	oldcontext = wglGetCurrentContext();
-	//wglMakeCurrent(hdc, imguiRenderContext);
-	imgui_newframe();
+	wglMakeCurrent(hdc, imguiRenderContext);
+
+	ImGui_ImplOpenGL2_NewFrame();
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
 	UpdateImGuiSettings();
@@ -660,8 +635,9 @@ void __stdcall RunImGui(HDC hdc) {
 	ImGui::Render();
 
 	// Draw the overlay
-	imgui_renderdrawdata(ImGui::GetDrawData());
-//	wglMakeCurrent(hdc, oldcontext);
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+	wglMakeCurrent(hdc, oldcontext);
 }
 
 
