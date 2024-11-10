3ed9cd1f499167e25c62c5606226b5b519a33fb7
namishere <50415197+namishere@users.noreply.github.com>
2024-11-09 21:21:29 -0800

    SoundStreamSet:[Get/Set]TargetVolumeMultiplier
    allows finer grain control over volume mixing

--- repentogon/LuaInterfaces/LuaSoundStreamSet.cpp
+++ repentogon/LuaInterfaces/LuaSoundStreamSet.cpp
@@ -2,6 +2,29 @@
 #include "LuaCore.h"
 #include "HookSystem.h"
 
+float musicVolumes[7] = { 1, 1, 1, 1, 1, 1, 1 };
+
+HOOK_METHOD(SoundStreamSet, game_constructor, () -> SoundStreamSet*) {
+	SoundStreamSet* set = super();
+
+	for (size_t i = 0; i < 7; i++) {
+		set->_streams[i]._unused = (uint8_t)i;
+	}
+
+	return set;
+}
+
+HOOK_METHOD(SoundStream, Update, (bool checkIfPlaying) -> void) {
+	// exclude jingle stream
+	if (this->_setRef) {
+		// main stream target volume isn't constantly recalculated like layers are
+		float target = this->_unused == 0 ? g_Manager->GetOptions()->_musicVolume : this->_targetVolume;
+		this->_targetVolume = target * musicVolumes[this->_unused];
+	}
+
+	super(checkIfPlaying);
+}
+
 LUA_FUNCTION(Lua_MusicManager_GetCurrentStreamSetId)
 {
 	Music* music = lua::GetUserdata<Music*>(L, 1, lua::Metatables::MUSIC_MANAGER, "MusicManager");
@@ -35,26 +58,16 @@ LUA_FUNCTION(Lua_MusicManager_GetStreamSet)
 	return 1;
 }
 
-LUA_FUNCTION(Lua_SoundStreamSetGetPrimaryStream)
-{
-	SoundStreamSet* set = *lua::GetUserdata<SoundStreamSet**>(L, 1, lua::metatables::SoundStreamSetMT);
-	SoundStream* toLua = &set->_stream;
-	SoundStream** luaStream = (SoundStream**)lua_newuserdata(L, sizeof(SoundStream*));
-	*luaStream = toLua;
-	luaL_setmetatable(L, lua::metatables::SoundStreamMT);
-	return 1;
-}
-
-LUA_FUNCTION(Lua_SoundStreamSetGetLayerStream)
+LUA_FUNCTION(Lua_SoundStreamSetGetStream)
 {
 	SoundStreamSet* set = *lua::GetUserdata<SoundStreamSet**>(L, 1, lua::metatables::SoundStreamSetMT);
 	int streamId = (int)luaL_checkinteger(L, 2);
 
-	if (streamId < 0 || streamId > 1) {
-		return luaL_error(L, "Invalid music ID %d. Min = 0, Max = 1", streamId);
+	if (streamId < 0 || streamId > 6) {
+		return luaL_error(L, "Invalid music ID %d. Min = 0, Max = 6", streamId);
 	}
 
-	SoundStream* toLua = &set->_stream;
+	SoundStream* toLua = &set->_streams[streamId];
 	SoundStream** luaStream = (SoundStream**)lua_newuserdata(L, sizeof(SoundStream*));
 	*luaStream = toLua;
 	luaL_setmetatable(L, lua::metatables::SoundStreamMT);
@@ -103,6 +116,35 @@ LUA_FUNCTION(Lua_SoundStreamSetIsPlaying)
 	return 1;
 }
 
+LUA_FUNCTION(Lua_SoundStreamSetGetTargetVolumeMultiplier)
+{
+	SoundStreamSet* set = *lua::GetUserdata<SoundStreamSet**>(L, 1, lua::metatables::SoundStreamSetMT);
+
+	int streamId = (int)luaL_checkinteger(L, 2);
+
+	if (streamId < 0 || streamId > 6) {
+		return luaL_error(L, "Invalid music ID %d. Min = 0, Max = 6", streamId);
+	}
+	lua_pushnumber(L, musicVolumes[streamId]);
+
+	return 1;
+}
+
+LUA_FUNCTION(Lua_SoundStreamSetSetTargetVolumeMultiplier)
+{
+	SoundStreamSet* set = *lua::GetUserdata<SoundStreamSet**>(L, 1, lua::metatables::SoundStreamSetMT);
+
+	int streamId = (int)luaL_checkinteger(L, 2);
+
+	if (streamId < 0 || streamId > 6) {
+		return luaL_error(L, "Invalid music ID %d. Min = 0, Max = 6", streamId);
+	}
+	
+	musicVolumes[streamId] = luaL_checknumber(L, 3);
+
+	return 0;
+}
+
 static void RegisterSoundStreamSet(lua_State* L)
 {
 	lua::RegisterFunction(L, lua::Metatables::MUSIC_MANAGER, "GetStreamSet", Lua_MusicManager_GetStreamSet);
@@ -110,14 +152,15 @@ static void RegisterSoundStreamSet(lua_State* L)
 	lua::RegisterFunction(L, lua::Metatables::MUSIC_MANAGER, "GetCurrentStreamSetId", Lua_MusicManager_GetCurrentStreamSetId);
 
 	luaL_Reg functions[] = {
-		{ "GetPrimaryStream", Lua_SoundStreamSetGetPrimaryStream},
-		{ "GetLayerStream", Lua_SoundStreamSetGetLayerStream},
+		{ "GetStream", Lua_SoundStreamSetGetStream},
 		{ "GetActiveLayers", Lua_SoundStreamSetGetActiveLayers},
-		{ "GetVolumeMode", Lua_SoundStreamSetGetVolumeMode},
+		{ "GetLayerMode", Lua_SoundStreamSetGetVolumeMode},
 		{ "GetVolume", Lua_SoundStreamSetGetVolume},
 		{ "GetTargetVolume", Lua_SoundStreamSetGetTargetVolume},
 		{ "GetFadeRate", Lua_SoundStreamSetGetFadeRate},
 		{ "IsPlaying", Lua_SoundStreamSetIsPlaying},
+		{ "GetTargetVolumeMultiplier", Lua_SoundStreamSetGetTargetVolumeMultiplier},
+		{ "SetTargetVolumeMultiplier", Lua_SoundStreamSetSetTargetVolumeMultiplier},
 		{ NULL, NULL }
 	};
 	lua::RegisterNewClass(L, lua::metatables::SoundStreamSetMT, lua::metatables::SoundStreamSetMT, functions);
