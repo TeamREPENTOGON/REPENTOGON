742e75571780ea7253761e2de12aaf14ccb29e09
Sylmir <sylmir.dev@gmail.com>
2025-06-12 23:13:55 +0200

    Exit with error code for launcher on mods changes

--- repentogon/Patches/ModReloading.h
+++ repentogon/Patches/ModReloading.h
@@ -3,20 +3,10 @@
 
 #include "Log.h"
 #include "../REPENTOGONOptions.h"
+#include "../LauncherInterface.h"
 
 inline void GameRestart() {
 	repentogonOptions.Write("internal", "DidModReset", "1");
 	g_Manager->GetOptions()->Save();
-	STARTUPINFO si = {};
-	PROCESS_INFORMATION pi = {};
-	LPSTR commandLine = GetCommandLine();
-	bool success = CreateProcess(NULL, commandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
-	if (!success) {
-		ZHL::Log("[ERROR] CreateProcess failed (GetLastError() = %d, cli = %s)\n", GetLastError(), commandLine);
-	}
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
-	if (success) {
-		TerminateProcess(GetCurrentProcess(), 0);
-	}
+	ExitProcess(LauncherInterface::LAUNCHER_EXIT_MODS_CHANGED);
 }
