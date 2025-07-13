9cbd18e250231073942c238ef10b0ee7f5caa0db
kittenchilly <mario.pikmin.zelda@gmail.com>
2025-07-13 13:56:02 -0500

    Expose every missing option

--- repentogon/LuaInterfaces/LuaOptions.cpp
+++ repentogon/LuaInterfaces/LuaOptions.cpp
@@ -96,6 +96,217 @@ LUA_FUNCTION(Lua_Options_SetOnlineInputDelay) {
 	return 0;
 }
 
+LUA_FUNCTION(Lua_Options_GetExposure) {
+	lua_pushnumber(L, (float)g_Manager->GetOptions()->_exposure);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetExposure) {
+	float value = (float)luaL_checknumber(L, 1);
+	g_Manager->GetOptions()->_exposure = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetBrightness) {
+	lua_pushnumber(L, (float)g_Manager->GetOptions()->_brightness);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetBrightness) {
+	float value = (float)luaL_checknumber(L, 1);
+	g_Manager->GetOptions()->_brightness = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetContrast) {
+	lua_pushnumber(L, (float)g_Manager->GetOptions()->_contrast);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetContrast) {
+	float value = (float)luaL_checknumber(L, 1);
+	g_Manager->GetOptions()->_contrast = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetControllerHotplugEnabled) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_controlHotplugEnabled);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetControllerHotplugEnabled) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_controlHotplugEnabled = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetTryImportSave) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_tryImportSave);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetTryImportSave) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_tryImportSave = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetBossHPOnBottom) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_bossHpOnBottom);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetBossHPOnBottom) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_bossHpOnBottom = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetModsEnabled) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_enableMods);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetModsEnabled) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_enableMods = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetTouchMode) {
+	lua_pushinteger(L, g_Manager->GetOptions()->_touchMode);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetTouchMode) {
+	int value = luaL_checkinteger(L, 1);
+	g_Manager->GetOptions()->_touchMode = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetAimLockEnabled) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_enableAimLock);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetAimLockEnabled) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_enableAimLock = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetAscentVoiceOver) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_ascentVoiceover);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetAscentVoiceOver) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_ascentVoiceover = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetOnlineChatFilterEnabled) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_onlineFilterEnabled);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetOnlineChatFilterEnabled) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_onlineFilterEnabled = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetAcceptedModDisclaimer) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_acceptedModDisclaimer);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetAcceptedModDisclaimer) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_acceptedModDisclaimer = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetAcceptedDataCollectionDisclaimer) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_acceptedDataCollectionDisclaimer);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetAcceptedDataCollectionDisclaimer) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_acceptedDataCollectionDisclaimer = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetAcceptedPublicBeta) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_acceptedPublicBeta_VERSION);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetAcceptedPublicBeta) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_acceptedPublicBeta_VERSION = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetSteamCloudEnabled) {
+	lua_pushboolean(L, g_Manager->GetOptions()->_enableSteamCloud);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetSteamCloudEnabled) {
+	bool value = lua::luaL_checkboolean(L, 1);
+	g_Manager->GetOptions()->_enableSteamCloud = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetWindowWidth) {
+	lua_pushinteger(L, g_Manager->GetOptions()->_windowWidth);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetWindowWidth) {
+	int value = luaL_checkinteger(L, 1);
+	g_Manager->GetOptions()->_windowWidth = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetWindowHeight) {
+	lua_pushinteger(L, g_Manager->GetOptions()->_windowHeight);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetWindowHeight) {
+	int value = luaL_checkinteger(L, 1);
+	g_Manager->GetOptions()->_windowHeight = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetWindowPosX) {
+	lua_pushinteger(L, g_Manager->GetOptions()->_windowPosX);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetWindowPosX) {
+	int value = luaL_checkinteger(L, 1);
+	value = std::min(std::max(8, value), 65535);
+	g_Manager->GetOptions()->_windowPosX = value;
+	return 0;
+}
+
+LUA_FUNCTION(Lua_Options_GetWindowPosY) {
+	lua_pushinteger(L, g_Manager->GetOptions()->_windowPosY);
+	return 1;
+}
+
+LUA_FUNCTION(Lua_Options_SetWindowPosY) {
+	int value = luaL_checkinteger(L, 1);
+	value = std::min(std::max(8, value), 65535);
+	g_Manager->GetOptions()->_windowPosY = value;
+	return 0;
+}
+
 LUA_FUNCTION(Lua_Options_GetVoidGeneration) {
 	lua_pushboolean(L, repentogonOptions.betterVoidGeneration);
 	return 1;
@@ -172,6 +383,25 @@ static void RegisterLuaOptions(lua_State* L) {
 	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "OnlinePlayerOpacity", Lua_Options_GetOnlinePlayerOpacity, Lua_Options_SetOnlinePlayerOpacity);
 	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "OnlineColorSet", Lua_Options_GetOnlineColorSet, Lua_Options_SetOnlineColorSet);
 	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "OnlineInputDelay", Lua_Options_GetOnlineInputDelay, Lua_Options_SetOnlineInputDelay);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "Exposure", Lua_Options_GetExposure, Lua_Options_SetExposure);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "Brightness", Lua_Options_GetBrightness, Lua_Options_SetBrightness);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "Contrast", Lua_Options_GetContrast, Lua_Options_SetContrast);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "ControllerHotplugEnabled", Lua_Options_GetControllerHotplugEnabled, Lua_Options_SetControllerHotplugEnabled);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "TryImportSave", Lua_Options_GetTryImportSave, Lua_Options_SetTryImportSave);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "BossHPOnBottom", Lua_Options_GetBossHPOnBottom, Lua_Options_SetBossHPOnBottom);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "ModsEnabled", Lua_Options_GetModsEnabled, Lua_Options_SetModsEnabled);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "TouchMode", Lua_Options_GetTouchMode, Lua_Options_SetTouchMode);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "AimLockEnabled", Lua_Options_GetAimLockEnabled, Lua_Options_SetAimLockEnabled);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "AscentVoiceOver", Lua_Options_GetAscentVoiceOver, Lua_Options_SetAscentVoiceOver);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "OnlineChatFilterEnabled", Lua_Options_GetOnlineChatFilterEnabled, Lua_Options_SetOnlineChatFilterEnabled);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "AcceptedModDisclaimer", Lua_Options_GetAcceptedModDisclaimer, Lua_Options_SetAcceptedModDisclaimer);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "AcceptedDataCollectionDisclaimer", Lua_Options_GetAcceptedDataCollectionDisclaimer, Lua_Options_SetAcceptedDataCollectionDisclaimer);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "AcceptedPublicBeta", Lua_Options_GetAcceptedPublicBeta, Lua_Options_SetAcceptedPublicBeta);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "SteamCloudEnabled", Lua_Options_GetSteamCloudEnabled, Lua_Options_SetSteamCloudEnabled);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "WindowWidth", Lua_Options_GetWindowWidth, Lua_Options_SetWindowWidth);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "WindowHeight", Lua_Options_GetWindowHeight, Lua_Options_SetWindowHeight);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "WindowPosX", Lua_Options_GetWindowPosX, Lua_Options_SetWindowPosX);
+	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "WindowPosY", Lua_Options_GetWindowPosY, Lua_Options_SetWindowPosY);
 	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "BetterVoidGeneration", Lua_Options_GetVoidGeneration, Lua_Options_SetVoidGeneration);
 	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "HushPanicStateFix", Lua_Options_GetHushFix, Lua_Options_SetHushFix);
 	lua::RegisterGlobalClassVariable(L, lua::GlobalClasses::Options, "StatHUDPlanetarium", Lua_Options_GetStatHUDPlanetarium, Lua_Options_SetStatHUDPlanetarium);
