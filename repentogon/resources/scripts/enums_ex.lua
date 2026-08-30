4ce4ab7710131364ea597baea362469bd51c9aab
Connor <connor@4n.ca>
2026-08-30 10:43:37 -0400

    Fix value of ModCallbacks.MC_POST_SHUFFLE_COSTUMES being wrong

--- repentogon/resources/scripts/enums_ex.lua
+++ repentogon/resources/scripts/enums_ex.lua
@@ -390,7 +390,7 @@ ModCallbacks.MC_PRE_OPEN_CHEST = 1491
 ModCallbacks.MC_POST_OPEN_CHEST = 1492
 ModCallbacks.MC_GET_BOSS_THEMATIC_ITEM = 1493
 ModCallbacks.MC_PRE_SHUFFLE_COSTUMES = 1494
-ModCallbacks.MC_POST_SHUFFLE_COSTUMES = 1945
+ModCallbacks.MC_POST_SHUFFLE_COSTUMES = 1495
 
 --Repentance+ Enums (🐱 catin: https://github.com/epfly6/RepentanceAPIIssueTracker/issues/597)
 EffectVariant.MEGA_BEAN_EXPLOSION = 202
